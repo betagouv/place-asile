b8c35d77b6b5777da2ad54e33bb920389b25de1b
Alezco <benjaminmorali4@hotmail.fr>
2025-02-07 18:49:09 +0100

    :sparkles: Add button component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Button = ({title}: Props) => {
+    return (
+        <button className="fr-btn">
+            {title}
+        </button>
+    )
+}
+
+type Props = {
+    title: string
+}
