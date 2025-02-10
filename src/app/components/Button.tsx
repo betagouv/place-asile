77142226f3f76e4012a70d71c2656aafc08d0dae
Alezco <benjaminmorali4@hotmail.fr>
2025-02-10 17:30:43 +0100

    :sparkles: Add leaflet map

--- src/app/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Button = ({ title }: Props) => {
-  return <button className="fr-btn">{title}</button>;
-};
-
-type Props = {
-  title: string;
-};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import { Button } from "./components/Button";
+"use client";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 export default function Home() {
-  return <Button title="TEST" />;
+  const CentresMap = useMemo(
+    () =>
+      dynamic(() => import("./components/CentresMap"), {
+        loading: () => <p>Chargement de la carte en cours...</p>,
+        ssr: false,
+      }),
+    []
+  );
+  return <CentresMap />;
 }
