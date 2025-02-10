b9bb7983ac1f7b973c97daa63a36ad66d21d1a05
Alezco <benjaminmorali4@hotmail.fr>
2025-02-10 15:31:55 +0100

    :sparkles: Add header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import styles from "./page.module.css";
 import { Button } from "./components/Button";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Button title="TEST" />
-      </main>
-    </div>
-  );
+  return <Button title="TEST" />;
 }
