a9e5b891db1bcc1d63bb3fe9ce112c692d73a732
Alezco <benjaminmorali4@hotmail.fr>
2025-02-10 15:31:55 +0100

    :sparkles: Add header and footer

---
+++ src/app/Header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header role="banner" className="fr-header">
+      <div className="fr-header__body">
+        <div className="fr-container">
+          <div className="fr-header__body-row">
+            <div className="fr-header__brand fr-enlarge-link">
+              <div className="fr-header__brand-top">
+                <div className="fr-header__logo">
+                  <p className="fr-logo">
+                    République
+                    <br />
+                    française
+                  </p>
+                </div>
+              </div>
+              <div className="fr-header__service">
+                <Link href="/" title="Accueil - Place d’asile">
+                  <p className="fr-header__service-title">Place d’asile</p>
+                </Link>
+                <p className="fr-header__service-tagline">
+                  Faciliter l’hébergement des demandeurs d’asile
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-export const Button = ({title}: Props) => {
-    return (
-        <button className="fr-btn">
-            {title}
-        </button>
-    )
-}
+export const Button = ({ title }: Props) => {
+  return <button className="fr-btn">{title}</button>;
+};
 
 type Props = {
-    title: string
-}
+  title: string;
+};
