4e1a8a364b525f7ed31e3302f4e84c8a19dca140
Alezco <benjaminmorali4@hotmail.fr>
2025-02-11 17:44:43 +0100

    :hammer: Add first version of migration script

---
+++ src/app/api/data/centres.data.ts
@@ -0,0 +1,17 @@
+export const centres = [
+  {
+    id: 1,
+    position: [46.6055983, 1.8750922],
+    name: "Centre 1",
+  },
+  {
+    id: 2,
+    position: [48.85341, 2.3488],
+    name: "Centre 2",
+  },
+  {
+    id: 3,
+    position: [43.85341, 1.1488],
+    name: "Centre 3",
+  },
+];
