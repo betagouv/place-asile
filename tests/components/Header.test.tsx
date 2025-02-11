f43e065b9b05829c2b2fd01ba4e4e1e827569b47
Alezco <benjaminmorali4@hotmail.fr>
2025-02-11 11:30:05 +0100

    :white_check_mark: Add first test

---
+++ tests/components/Header.test.tsx
@@ -0,0 +1,12 @@
+import { expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Header } from "../../src/app/components/Header";
+
+it("Header", () => {
+  // WHEN
+  render(<Header />);
+
+  // THEN
+  const title = screen.getByRole("link", { name: "Place d’asile" });
+  expect(title).toBeDefined();
+});
