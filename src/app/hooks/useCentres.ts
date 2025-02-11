a7e8312a5721ca43db98a4c9e4973ff53663be62
Alezco <benjaminmorali4@hotmail.fr>
2025-02-11 16:33:00 +0100

    :art: Reformat ts hook

--- src/app/hooks/useCentres.ts
+++ src/app/hooks/useCentres.ts
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export const useCentres = () => {
-    const [centres, setCentres] = useState([]);
-    useEffect(() => {
-        const getCentres = async () => {
-            const result = await fetch("/api/centres")
-            const centres = await result.json()
-            setCentres(centres);
-        }
-        getCentres()
-    }, [])
+  const [centres, setCentres] = useState([]);
+  useEffect(() => {
+    const getCentres = async () => {
+      const result = await fetch("/api/centres");
+      const centres = await result.json();
+      setCentres(centres);
+    };
+    getCentres();
+  }, []);
 
-    return centres
-}
+  return centres;
+};
