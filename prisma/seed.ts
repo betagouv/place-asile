19a622604e86c848baf3c9c3769b81d0a8ae8e51
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-02-25 19:02:24 +0100

    :recycle: Call migration script from db seed (#10)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from "@prisma/client";
-import centres from "../scripts/export.json";
+import { extractFromXslx } from "../scripts/extract-from-xlsx.js";
+import { CentreWithCoordinates } from "@/types/centre.type.js";
 
 const prisma = new PrismaClient();
 
 export async function seed() {
+  const centres = await extractFromXslx(false);
   for (const centre of centres) {
     const sanitizedCentre = {
       ...centre,
       codePostalHebergement: centre.codePostalHebergement.toString(),
     };
-    await prisma.structure.create({ data: sanitizedCentre });
+    await prisma.structure.create({
+      data: sanitizedCentre as CentreWithCoordinates,
+    });
   }
 }
 
