07cea0888532091cee8ca4970ffc428a4c7843b7
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-02-25 17:47:14 +0100

    :green_heart: Fix next build (#8)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,12 @@ import { Footer } from "./components/Footer";
 import { Menu } from "./components/Menu";
 
 import "@gouvfr/dsfr/dist/dsfr.min.css";
-import "@gouvfr/dsfr/dist/utility/icons/icons.min.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-buildings/icons-buildings.min.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-business/icons-business.min.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-document/icons-document.min.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-map/icons-map.min.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.min.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-user/icons-user.min.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
