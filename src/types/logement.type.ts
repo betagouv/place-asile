17e58488d2b3905a68dadbd78ef35baead6a7041
Alezco <benjaminmorali4@hotmail.fr>
2025-03-10 16:49:05 +0100

    :bug: Fix repartition by commune

--- src/types/logement.type.ts
+++ src/types/logement.type.ts
@@ -6,4 +6,5 @@ export type Logement = {
   ville: string;
   qpv: boolean;
   logementSocial: boolean;
+  nbPlaces: number;
 };
--- tests/utils/structure.util.test.ts
+++ tests/utils/structure.util.test.ts
@@ -1,10 +1,10 @@
 import { getPlacesByCommunes } from "@/app/utils/structure.util";
 import { Logement } from "../../src/types/logement.type";
+import { createLogement } from "../test-utils/logement.factory";
 
-// TODO : implement this test
-describe.todo("structure util", () => {
+describe("structure util", () => {
   describe("getPlacesByCommunes", () => {
-    it("should return 0 when given an empty array", () => {
+    it("should return an empty object when given an empty array", () => {
       // GIVEN
       const logements: Logement[] = [];
 
@@ -12,17 +12,22 @@ describe.todo("structure util", () => {
       const placesByCommune = getPlacesByCommunes(logements);
 
       // THEN
-      expect(placesByCommune).toStrictEqual(0);
+      expect(placesByCommune).toStrictEqual({});
     });
-    it("should return the sum of nbPlaces when given a structures array", () => {
+    it("should return correct places by commune when given a logements array", () => {
       // GIVEN
-      const logements: Logement[] = [];
+      const logements: Logement[] = [
+        createLogement({ ville: "Paris", nbPlaces: 3 }),
+        createLogement({ ville: "Paris", nbPlaces: 2 }),
+        createLogement({ ville: "Rouen", nbPlaces: 1 }),
+        createLogement({ ville: "Rouen", nbPlaces: 1 }),
+      ];
 
       // WHEN
       const placesByCommune = getPlacesByCommunes(logements);
 
       // THEN
-      expect(placesByCommune).toStrictEqual(18);
+      expect(placesByCommune).toStrictEqual({ Paris: 5, Rouen: 2 });
     });
   });
 });
