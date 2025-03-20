0412a5f691a9b30316ab81e19e1bb6fe636179d4
Alezco <benjaminmorali4@hotmail.fr>
2025-03-20 18:21:46 +0100

    :sparkles: Add type places block

--- src/app/(authenticated)/structures/[id]/TypePlaceBlock.tsx
+++ src/app/(authenticated)/structures/[id]/TypePlaceBlock.tsx
@@ -1,6 +1,20 @@
 import { Block } from "@/app/components/common/Block";
+import { InformationCard } from "@/app/components/InformationCard";
 import { ReactElement } from "react";
+import { PieChart } from "@/app/components/common/PieChart";
 
-export const TypePlaceBlock = (): ReactElement => {
-  return <Block title="Type de place" iconClass="fr-icon-map-pin-2-line" />;
+export const TypePlaceBlock = ({ placesAutorisees }: Props): ReactElement => {
+  return (
+    <Block title="Type de place" iconClass="fr-icon-map-pin-2-line">
+      <InformationCard
+        primaryInformation={placesAutorisees}
+        secondaryInformation="places autorisées"
+      />
+      <PieChart />
+    </Block>
+  );
+};
+
+type Props = {
+  placesAutorisees: number;
 };
