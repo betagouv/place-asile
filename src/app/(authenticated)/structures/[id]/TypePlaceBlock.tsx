97f6d6e6c5dc0e6c237033f34e85756fab9d8ef6
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-03-27 17:10:17 +0100

    :heavy_plus_sign: Replace dsfr with react-dsfr (#42)

--- src/app/(authenticated)/structures/[id]/TypePlaceBlock.tsx
+++ src/app/(authenticated)/structures/[id]/TypePlaceBlock.tsx
@@ -1,6 +1,20 @@
+"use client";
+
 import { Block } from "@/app/components/common/Block";
 import { ReactElement } from "react";
+import dynamic from "next/dynamic";
+
+const PieChart = dynamic(
+  () => import("@codegouvfr/react-dsfr/Chart/PieChart"),
+  {
+    ssr: false,
+  }
+);
 
 export const TypePlaceBlock = (): ReactElement => {
-  return <Block title="Type de place" iconClass="fr-icon-map-pin-2-line" />;
+  return (
+    <Block title="Type de place" iconClass="fr-icon-map-pin-2-line">
+      <PieChart x={[1, 2]} y={[2, 3]} />
+    </Block>
+  );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
 import { PropsWithChildren } from "react";
-
-import "@gouvfr/dsfr/dist/dsfr.min.css";
-import "@gouvfr/dsfr/dist/utility/icons/icons-buildings/icons-buildings.min.css";
-import "@gouvfr/dsfr/dist/utility/icons/icons-business/icons-business.min.css";
-import "@gouvfr/dsfr/dist/utility/icons/icons-document/icons-document.min.css";
-import "@gouvfr/dsfr/dist/utility/icons/icons-map/icons-map.min.css";
-import "@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.min.css";
-import "@gouvfr/dsfr/dist/utility/icons/icons-user/icons-user.min.css";
-import "./globals.css";
 import { Providers } from "./Providers";
+import {
+  DsfrHead,
+  getHtmlAttributes,
+} from "./dsfr-bootstrap/server-only-index";
+import { DsfrProvider } from "./dsfr-bootstrap";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Place d'asile",
@@ -17,10 +14,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
+  const lang = "fr";
   return (
-    <html data-fr-scheme="light" dir="ltr" lang="fr">
+    <html {...getHtmlAttributes({ lang })} dir="ltr">
+      <head>
+        <DsfrHead />
+      </head>
       <body>
-        <Providers>{children}</Providers>
+        <DsfrProvider lang={lang}>
+          <Providers>{children}</Providers>
+        </DsfrProvider>
       </body>
     </html>
   );
