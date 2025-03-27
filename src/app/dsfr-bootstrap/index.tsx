dd27f8187dbdc185dd21b78b670f67b87eb406d0
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-03-27 17:10:17 +0100

    :heavy_plus_sign: Replace dsfr with react-dsfr (#42)

---
+++ src/app/dsfr-bootstrap/index.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import {
+  DsfrProviderBase,
+  StartDsfrOnHydration,
+} from "@codegouvfr/react-dsfr/next-app-router";
+import { defaultColorScheme } from "./defaultColorScheme";
+import Link from "next/link";
+
+declare module "@codegouvfr/react-dsfr/next-app-router" {
+  interface RegisterLink {
+    Link: typeof Link;
+  }
+}
+
+export function DsfrProvider(props: {
+  lang: string | undefined;
+  children: React.ReactNode;
+}) {
+  const { lang, children } = props;
+  return (
+    <DsfrProviderBase
+      lang={lang}
+      defaultColorScheme={defaultColorScheme}
+      Link={Link}
+    >
+      {children}
+    </DsfrProviderBase>
+  );
+}
+
+export { StartDsfrOnHydration };
