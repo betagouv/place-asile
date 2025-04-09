98f7f5d098ced783d5a51e21b4ad73d2f0ffe9c9
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-04-09 13:22:46 +0200

    :sparkles: Add motifs indisponibilite (#62)

--- src/app/(authenticated)/structures/[id]/ActivitesBlock.tsx
+++ src/app/(authenticated)/structures/[id]/ActivitesBlock.tsx
@@ -2,6 +2,7 @@ import { Block } from "@/app/components/common/Block";
 import { Activite } from "@/types/activite.type";
 import { ReactElement } from "react";
 import { ActivitesPlaces } from "./ActivitesPlaces";
+import { ActivitesMotifsIndisponibilite } from "./ActivitesMotifsIndisponibilite";
 
 export const ActivitesBlock = ({ activites }: Props): ReactElement => {
   return (
@@ -10,13 +11,24 @@ export const ActivitesBlock = ({ activites }: Props): ReactElement => {
         Données mensuelles mises à jour le{" "}
         {new Date(activites?.[0]?.date).toLocaleDateString()}
       </span>
-      <ActivitesPlaces
-        nbPlaces={activites[0].nbPlaces}
-        placesIndisponibles={activites[0].placesIndisponibles}
-        placesVacantes={activites[0].placesVacantes}
-        placesPIBPI={activites[0].placesPIBPI}
-        placesPIdeboutees={activites[0].placesPIdeboutees}
-      />
+      <div className="fr-pb-3w">
+        <ActivitesPlaces
+          nbPlaces={activites[0]?.nbPlaces}
+          placesIndisponibles={activites[0]?.placesIndisponibles}
+          placesVacantes={activites[0]?.placesVacantes}
+          placesPIBPI={activites[0]?.placesPIBPI}
+          placesPIdeboutees={activites[0]?.placesPIdeboutees}
+        />
+      </div>
+      <div className="fr-pb-3w">
+        <ActivitesMotifsIndisponibilite
+          desinsectisation={activites[0]?.desinsectisation}
+          remiseEnEtat={activites[0]?.remiseEnEtat}
+          sousOccupation={activites[0]?.sousOccupation}
+          travaux={activites[0]?.travaux}
+          placesHorsDnaNg={activites[0]?.placesHorsDnaNg}
+        />
+      </div>
     </Block>
   );
 };
