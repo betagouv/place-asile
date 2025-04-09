74f4dd856f95db175dbfd8b2e449cfbad59ed910
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-04-09 13:22:46 +0200

    :sparkles: Add motifs indisponibilite (#62)

---
+++ src/app/(authenticated)/structures/[id]/ActivitesMotifsIndisponibilite.tsx
@@ -0,0 +1,44 @@
+import Accordion from "@codegouvfr/react-dsfr/Accordion";
+import { ReactElement } from "react";
+import styles from "../../../components/common/Accordion.module.css";
+import Table from "@codegouvfr/react-dsfr/Table";
+
+export const ActivitesMotifsIndisponibilite = ({
+  desinsectisation,
+  remiseEnEtat,
+  sousOccupation,
+  travaux,
+  placesHorsDnaNg,
+}: Props): ReactElement => {
+  const getTableData = () => {
+    return [
+      ["Désinsectisation", desinsectisation],
+      ["Remise en état de l'unité", remiseEnEtat],
+      ["Sous-occupation", sousOccupation],
+      ["Travaux", travaux],
+      ["Public hors DNA-NG", placesHorsDnaNg],
+    ];
+  };
+  return (
+    <Accordion
+      label="Motifs d'indisponibilité"
+      className={styles["custom-accordion"]}
+    >
+      <Table
+        bordered={true}
+        className="fr-m-0"
+        caption=""
+        data={getTableData()}
+        headers={["MOTIF", "PLACES CONCERNÉES"]}
+      />
+    </Accordion>
+  );
+};
+
+type Props = {
+  desinsectisation: number;
+  remiseEnEtat: number;
+  sousOccupation: number;
+  travaux: number;
+  placesHorsDnaNg: number;
+};
