103157b3ca9f95c7559f4bdcf6dcfda69dc92649
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-04-10 11:39:40 +0200

    :sparkles: Ajout structure public route + password protction

---
+++ src/app/api/mot-de-passe/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { password } = await request.json();
+
+  if (password === process.env.PAGE_PASSWORD) {
+    const res = NextResponse.json({ success: true });
+    res.cookies.set("mot-de-passe", password, {
+      httpOnly: true,
+      path: "/",
+      maxAge: 60 * 60 * 24, // 1 day
+    });
+    return res;
+  }
+
+  return NextResponse.json({ success: false }, { status: 401 });
+}
