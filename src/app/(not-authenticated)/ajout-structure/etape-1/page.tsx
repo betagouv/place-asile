5e43731032cf3951a7729c5affe0968355b66428
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-04-17 14:41:01 +0200

    :art: Code refacto + new input with errors component

--- src/app/(not-authenticated)/ajout-structure/etape-1/page.tsx
+++ src/app/(not-authenticated)/ajout-structure/etape-1/page.tsx
@@ -2,50 +2,47 @@
 "use client";
 
 import { useForm, useStore } from "@tanstack/react-form";
-import { step1Schema } from "../validation/validation";
-import Input from "@codegouvfr/react-dsfr/Input";
 import Button from "@codegouvfr/react-dsfr/Button";
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import Stepper from "@codegouvfr/react-dsfr/Stepper";
 import { useRouter } from "next/navigation";
+import { useLocalStorage } from "@/app/hooks/useLocalStorage";
+import { step1Schema } from "../validation/validation";
+import InputWithValidation from "@/app/components/forms/InputWithValidation";
 
 export default function Step1Form() {
   const router = useRouter();
-  // The current localStorage values
-  const localStorageValues = useMemo(() => {
-    if (typeof window !== "undefined") {
-      return JSON.parse(localStorage?.getItem("ajout-structure") || "{}");
-    }
-    return {};
-  }, []);
+  const [localStorageValues, updateLocalStorageValues] = useLocalStorage(
+    "ajout-structure",
+    {}
+  );
 
   const form = useForm({
-    onSubmit: async ({ value }) => {
-      console.log(value);
+    onSubmit: async ({}) => {
       router.push("/ajout-structure/etape-2");
     },
 
     defaultValues: {
-      prenom: localStorageValues.prenom || "",
-      nom: localStorageValues.nom || "",
-      email: localStorageValues.email || "",
+      prenom: localStorageValues?.prenom || "",
+      nom: localStorageValues?.nom || "",
+      email: localStorageValues?.email || "",
     },
     validators: {
-      onChange: step1Schema,
+      onSubmit: (values) => {
+        const result = step1Schema.safeParse(values);
+        if (!result.success) {
+          return result.error.formErrors.fieldErrors;
+        }
+        return;
+      },
     },
   });
 
   const formState = useStore(form.store, (state) => state.values);
 
   useEffect(() => {
-    console.log(formState);
-    if (typeof window !== "undefined") {
-      localStorage.setItem(
-        "ajout-structure",
-        JSON.stringify({ ...formState, ...localStorageValues })
-      );
-    }
-  }, [formState, localStorageValues]);
+    updateLocalStorageValues(formState);
+  }, [formState, updateLocalStorageValues]);
   return (
     <div className="fr-container mx-auto my-10">
       <Stepper
@@ -53,7 +50,7 @@ export default function Step1Form() {
         nextTitle="Étape 2"
         stepCount={3}
         title="Étape 1"
-      />{" "}
+      />
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -61,108 +58,31 @@ export default function Step1Form() {
           form.handleSubmit();
         }}
       >
-        <form.Field
+        <InputWithValidation
           name="prenom"
-          children={(field) => {
-            console.log(field.state.meta.errors);
-            return (
-              <>
-                <Input
-                  nativeInputProps={{
-                    name: "prenom",
-                    type: "text",
-                    required: true,
-                    value: form.state.values.prenom,
-                    onChange: (e) => {
-                      if (typeof window !== "undefined") {
-                        localStorageValues.prenom = e.target.value;
-                        localStorage.setItem(
-                          "ajout-structure",
-                          JSON.stringify(localStorageValues)
-                        );
-                      }
-                      field.handleChange(e.target.value);
-                    },
-                  }}
-                  label="Prénom"
-                  state={
-                    field.state.meta.errors?.length > 0 ? "error" : "default"
-                  }
-                  stateRelatedMessage={field.state.meta.errors
-                    ?.map((error) => error && error.message)
-                    .join("\n")}
-                />
-              </>
-            );
-          }}
+          type="text"
+          label="Prénom"
+          form={form}
+          schema={step1Schema}
+          localstorageKey="ajout-structure"
         />
-        <form.Field
+
+        <InputWithValidation
           name="nom"
-          children={(field) => {
-            return (
-              <>
-                <Input
-                  nativeInputProps={{
-                    name: "nom",
-                    type: "text",
-                    required: true,
-                    value: form.state.values.nom,
-                    onChange: (e) => {
-                      if (typeof window !== "undefined") {
-                        localStorageValues.nom = e.target.value;
-                        localStorage.setItem(
-                          "ajout-structure",
-                          JSON.stringify(localStorageValues)
-                        );
-                      }
-                      field.handleChange(e.target.value);
-                    },
-                  }}
-                  label="Nom"
-                  state={
-                    field.state.meta.errors?.length > 0 ? "error" : "default"
-                  }
-                  stateRelatedMessage={field.state.meta.errors
-                    ?.map((error) => error && error.message)
-                    .join("\n")}
-                />
-              </>
-            );
-          }}
+          type="text"
+          label="Nom"
+          form={form}
+          schema={step1Schema}
+          localstorageKey="ajout-structure"
         />
-        <form.Field
+
+        <InputWithValidation
           name="email"
-          children={(field) => {
-            return (
-              <>
-                <Input
-                  nativeInputProps={{
-                    name: "email",
-                    type: "email",
-                    required: true,
-                    value: form.state.values.email,
-                    onChange: (e) => {
-                      if (typeof window !== "undefined") {
-                        localStorageValues.email = e.target.value;
-                        localStorage.setItem(
-                          "ajout-structure",
-                          JSON.stringify(localStorageValues)
-                        );
-                      }
-                      field.handleChange(e.target.value);
-                    },
-                  }}
-                  label="Email"
-                  state={
-                    field.state.meta.errors?.length > 0 ? "error" : "default"
-                  }
-                  stateRelatedMessage={field.state.meta.errors
-                    ?.map((error) => error && error.message)
-                    .join("\n")}
-                />
-              </>
-            );
-          }}
+          type="email"
+          label="Email"
+          form={form}
+          schema={step1Schema}
+          localstorageKey="ajout-structure"
         />
 
         <form.Subscribe
