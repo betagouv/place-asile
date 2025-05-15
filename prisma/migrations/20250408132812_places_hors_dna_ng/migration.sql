b436e9948e43897e9196b2f9c7913d1a8e6b1a4d
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-05-15 15:58:19 +0200

    :card_file_box: Merge all migrations (#113)

--- prisma/migrations/20250408132812_places_hors_dna_ng/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `placesHorsDnaNg` to the `Activite` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Activite" ADD COLUMN     "placesHorsDnaNg" INTEGER NOT NULL;
--- prisma/migrations/20250506150918_notes/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Structure" ADD COLUMN     "notes" TEXT;
