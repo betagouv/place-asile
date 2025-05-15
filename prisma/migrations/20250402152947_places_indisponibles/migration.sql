8333a0b468997c203f0cdef8d4d809fbbbfa52ca
Alezco <benjaminmorali4@hotmail.fr>
2025-05-15 15:33:00 +0200

    :card_file_box: Merge all migrations

--- prisma/migrations/20250402152947_places_indisponibles/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `placesIndisponibles` to the `Activite` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Activite" ADD COLUMN     "placesIndisponibles" INTEGER NOT NULL;
