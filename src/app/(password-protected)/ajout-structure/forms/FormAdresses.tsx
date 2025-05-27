f0a6c0f748a2b1e0e814c1b91ecb7dec53d71afd
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-05-27 14:57:07 +0200

    :bug: Adresses fixes (#190)
    * :bug: Adresses fixes
    
    * :art: Get the content back

--- src/app/(password-protected)/ajout-structure/forms/FormAdresses.tsx
+++ src/app/(password-protected)/ajout-structure/forms/FormAdresses.tsx
@@ -1,10 +1,7 @@
 "use client";
 import FormWrapper from "@/app/components/forms/FormWrapper";
 import React, { useState, useEffect, useMemo } from "react";
-import {
-  AdressesSchemaFlexible,
-  AdressesSchemaStrict,
-} from "../validation/validation";
+import { AdressesSchema } from "../validation/validation";
 import { useParams, useSearchParams } from "next/navigation";
 import Link from "next/link";
 import InputWithValidation from "@/app/components/forms/InputWithValidation";
@@ -13,13 +10,9 @@ import AddressWithValidation from "@/app/components/forms/AddressWithValidation"
 import SelectWithValidation from "@/app/components/forms/SelectWithValidation";
 import { Repartition } from "@/types/adresse.type";
 import { Notice } from "@codegouvfr/react-dsfr/Notice";
-import { Checkbox } from "@codegouvfr/react-dsfr/Checkbox";
-import Button from "@codegouvfr/react-dsfr/Button";
 import { useRef } from "react";
 import autoAnimate from "@formkit/auto-animate";
-import { Controller } from "react-hook-form";
-import { MODELE_DIFFUS_LINK, MODELE_MIXTE_LINK } from "@/constants";
-import { AdressImporter } from "../[dnaCode]/02-adresses/AdressImporter";
+import AdressesList from "../[dnaCode]/02-adresses/AdressesList";
 
 export default function FormAdresses() {
   const params = useParams();
@@ -47,20 +40,7 @@ export default function FormAdresses() {
       codePostalAdministratif: "",
       communeAdministrative: "",
       departementAdministratif: "",
-      typeBati: Repartition.COLLECTIF,
-      adresses: [
-        {
-          adresseComplete: "",
-          adresse: "",
-          codePostal: "",
-          commune: "",
-          departement: "",
-          repartition: Repartition.COLLECTIF,
-          places: 0,
-          logementSocial: false,
-          qpv: false,
-        },
-      ],
+      typeBati: undefined,
     }),
     []
   );
@@ -75,31 +55,20 @@ export default function FormAdresses() {
     return {
       ...defaultValues,
       ...localStorageValues,
-      adresses: localStorageValues.adresses?.length
-        ? localStorageValues.adresses
-        : defaultValues.adresses,
     };
   }, [localStorageValues, defaultValues]);
 
-  const [showExtraFields, setShowExtraFields] = useState(false);
-
   const [isInitialized, setIsInitialized] = useState(false);
-
-  const selectedSchema = useMemo(
-    () => (showExtraFields ? AdressesSchemaStrict : AdressesSchemaFlexible),
-    [showExtraFields]
-  );
 
   useEffect(() => {
     if (localStorageValues && !isInitialized) {
-      setShowExtraFields(localStorageValues.typeBati !== Repartition.COLLECTIF);
       setIsInitialized(true);
     }
   }, [localStorageValues, isInitialized]);
 
   return (
     <FormWrapper
-      schema={selectedSchema}
+      schema={AdressesSchema}
       localStorageKey={`ajout-structure-${params.dnaCode}-adresses`}
       nextRoute={nextRoute}
       mode="onBlur"
@@ -109,39 +78,49 @@ export default function FormAdresses() {
       }
     >
       {({ control, setValue, getValues, watch }) => {
-        const typeBati = watch("typeBati");
-        const handleAddAddress = () => {
-          if (!selectedSchema) {
-            return;
-          }
-          const newAddress = {
-            adresseComplete: "",
-            adresse: "",
-            codePostal: "",
-            commune: "",
-            departement: "",
-            repartition: Repartition.DIFFUS,
-            places: 0,
-            logementSocial: false,
-            qpv: false,
-          };
-          const currentAddresses = getValues("adresses") || [];
-          const updatedAddresses = [...currentAddresses, newAddress];
-          setValue("adresses", updatedAddresses, {
-            shouldValidate: false,
-          });
-        };
+        const typeBati = watch("typeBati") as Repartition | undefined;
 
-        const handleRemoveAddress = (index: number) => {
-          if (!selectedSchema) {
-            return;
+        const handleTypeBatiChange = (value: string) => {
+          if (
+            value === Repartition.COLLECTIF ||
+            value === undefined ||
+            value === ""
+          ) {
+            // TODO: @ledjay mettre en place une confirmation avant de supprimer les adresses
+            setValue("adresses", [], { shouldValidate: false });
+          } else {
+            const currentAdresses = getValues("adresses") || [];
+            if (currentAdresses.length === 0) {
+              setValue(
+                "adresses",
+                [
+                  {
+                    adresseComplete: "",
+                    adresse: "",
+                    codePostal: "",
+                    commune: "",
+                    departement: "",
+                    repartition: value as Repartition,
+                    places: 0,
+                    logementSocial: false,
+                    qpv: false,
+                  },
+                ],
+                { shouldValidate: false }
+              );
+            } else {
+              const currentAdresses = getValues("adresses") || [];
+
+              const updatedAdresses = currentAdresses.map((adresse) => ({
+                ...adresse,
+                repartition: value as Repartition,
+              }));
+
+              setValue("adresses", updatedAdresses, {
+                shouldValidate: false,
+              });
+            }
           }
-          const currentAddresses = getValues("adresses") || [];
-          const updatedAddresses = [...currentAddresses];
-          updatedAddresses.splice(index, 1);
-          setValue("adresses", updatedAddresses, {
-            shouldValidate: false,
-          });
         };
 
         return (
@@ -191,23 +170,7 @@ export default function FormAdresses() {
                   name="typeBati"
                   control={control}
                   label="Type de bati"
-                  onChange={(value) => {
-                    setShowExtraFields(value !== Repartition.COLLECTIF);
-                    if (value === Repartition.DIFFUS) {
-                      const currentAddresses = getValues("adresses") || [];
-                      const updatedAddresses = currentAddresses.map(
-                        (adresse) => {
-                          return {
-                            ...adresse,
-                            repartition: Repartition.DIFFUS,
-                          };
-                        }
-                      );
-                      setValue("adresses", updatedAddresses, {
-                        shouldValidate: false,
-                      });
-                    }
-                  }}
+                  onChange={handleTypeBatiChange}
                   required
                 >
                   <option value="">Sélectionnez une option</option>
@@ -223,189 +186,16 @@ export default function FormAdresses() {
 
             <div ref={hebergementsContainerRef}>
               {/* Only render on client-side and when condition is met */}
-              {isInitialized && typeBati !== Repartition.COLLECTIF && (
-                <>
-                  <hr />
-                  <fieldset className="flex flex-col gap-6">
-                    <legend className="text-xl font-bold mb-4 text-title-blue-france">
-                      Hébergements
-                    </legend>
-                    <div className="flex flex-col gap-2">
-                      <p className="mb-1">
-                        Veuillez renseigner l’ensemble des adresses
-                        d’hébergement de la structure. <br />
-                        Vous pouvez le faire directement en remplissant les
-                        champs ci-dessous ou vous pouvez compléter{" "}
-                        <a
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          href={
-                            typeBati === Repartition.DIFFUS
-                              ? MODELE_DIFFUS_LINK
-                              : MODELE_MIXTE_LINK
-                          }
-                          className="underline"
-                        >
-                          notre modèle à télécharger
-                        </a>{" "}
-                        depuis un logiciel tableur, l’importer puis vérifier le
-                        remplissage automatique des champs qui s’opérera.
-                      </p>
-                      <div className="flex flex-col gap-2">
-                        <p className="text-action-high-blue-france font-bold mb-0">
-                          Liste des hébergements (d’après notre modèle à
-                          télécharger uniquement)
-                        </p>
-                        <p className="text-disabled-grey mb-0 text-xs col-span-3">
-                          Taille maximale par fichier : 10 Mo. Formats supportés
-                          : xls, xlsx, et csv.
-                        </p>
-                        <AdressImporter
-                          getValues={getValues}
-                          setValue={setValue}
-                          typeBati={typeBati}
-                        />
-                      </div>
-                    </div>
-                    <Notice
-                      severity="info"
-                      title="Pour le champ “places”,"
-                      description="veuillez renseigner le nombre total de places autorisées pour l’adresse correspondante."
-                    />
-                    <Notice
-                      severity="info"
-                      title=""
-                      description={
-                        <>
-                          Vous pouvez vérifier si une adresse fait partie d’ un
-                          Quartier Prioritaire de la politique de la Ville (QPV){" "}
-                          <a
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href="https://sig.ville.gouv.fr/"
-                            className="fr-link fr-icon border-b w-fit pb-px hover:pb-0 hover:border-b-2"
-                          >
-                            sur ce lien.
-                          </a>{" "}
-                          Si une adresse ne donne pas de résultat, veuillez
-                          laisser la case décochée.
-                        </>
-                      }
-                    />
-
-                    {(getValues("adresses") || []).map((_, index) => (
-                      <div
-                        className="flex max-sm:flex-col gap-6"
-                        key={`address-${index}`}
-                      >
-                        <AddressWithValidation
-                          id={`adresses.${index}.adresseComplete`}
-                          control={control}
-                          fullAddress={`adresses.${index}.adresseComplete`}
-                          zipCode={`adresses.${index}.codePostal`}
-                          street={`adresses.${index}.adresse`}
-                          department={`adresses.${index}.departement`}
-                          city={`adresses.${index}.commune`}
-                          latitude={`adresses.${index}.latitude`}
-                          longitude={`adresses.${index}.longitude`}
-                          label="Adresse"
-                          className="w-1/3"
-                        />
-                        <InputWithValidation
-                          name={`adresses.${index}.places`}
-                          id={`adresses.${index}.places`}
-                          control={control}
-                          type="number"
-                          min={0}
-                          label="Places"
-                          className="w-1/12 mb-0"
-                        />
-                        <SelectWithValidation
-                          name={`adresses.${index}.repartition`}
-                          id={`adresses.${index}.repartition`}
-                          control={control}
-                          label="Type de bâti"
-                          hidden={typeBati === Repartition.DIFFUS}
-                          required
-                        >
-                          <option value="">Sélectionnez une option</option>
-                          {Object.values(Repartition).map((repartition) => (
-                            <option key={repartition} value={repartition}>
-                              {repartition}
-                            </option>
-                          ))}
-                        </SelectWithValidation>
-                        <div className="flex grow flex-col gap-2">
-                          <label htmlFor={`adresses.${index}.typologies`}>
-                            Particularités
-                          </label>
-                          <div className="flex w-full gap-4 items-center min-h-[2.6rem]">
-                            <Controller
-                              control={control}
-                              name={`adresses.${index}.logementSocial`}
-                              render={({ field }) => (
-                                <Checkbox
-                                  options={[
-                                    {
-                                      label: "Logement social",
-                                      nativeInputProps: {
-                                        name: field.name,
-                                        checked: field.value,
-                                        onChange: field.onChange,
-                                      },
-                                    },
-                                  ]}
-                                />
-                              )}
-                            />
-                            <Controller
-                              control={control}
-                              name={`adresses.${index}.qpv`}
-                              render={({ field }) => (
-                                <Checkbox
-                                  options={[
-                                    {
-                                      label: "QPV",
-                                      nativeInputProps: {
-                                        name: field.name,
-                                        checked: field.value,
-                                        onChange: field.onChange,
-                                      },
-                                    },
-                                  ]}
-                                />
-                              )}
-                            />
-
-                            {index !== 0 && (
-                              <Button
-                                iconId="fr-icon-delete-line"
-                                className="ml-auto rounded-4xl"
-                                onClick={(e) => {
-                                  e.preventDefault();
-                                  handleRemoveAddress(index);
-                                }}
-                                priority="tertiary no outline"
-                                title="Label button"
-                              />
-                            )}
-                          </div>
-                        </div>
-                      </div>
-                    ))}
-
-                    <button
-                      onClick={(e) => {
-                        e.preventDefault();
-                        handleAddAddress();
-                      }}
-                      className="fr-link fr-icon border-b w-fit pb-px hover:pb-0 hover:border-b-2"
-                    >
-                      + Ajouter un hébergement
-                    </button>
-                  </fieldset>
-                </>
-              )}
+              {isInitialized &&
+                typeBati !== undefined &&
+                typeBati !== Repartition.COLLECTIF && (
+                  <AdressesList
+                    watch={watch}
+                    control={control}
+                    setValue={setValue}
+                    getValues={getValues}
+                  />
+                )}
             </div>
           </>
         );
