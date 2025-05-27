c88a5fb16e828138a0782693d7a274839dd5d056
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-05-27 15:42:43 +0200

    :sparkles: Add email script (#191)

---
+++ scripts/generate-email-csv.ts
@@ -0,0 +1,40 @@
+import xlsx from "node-xlsx";
+import fs from "fs";
+
+const sheets = xlsx.parse(`${__dirname}/operateurs.xlsx`, {
+  cellDates: true,
+  blankrows: false,
+});
+
+const sheet = sheets[2].data;
+sheet.shift();
+sheet.shift();
+sheet.shift();
+sheet.shift();
+
+const structures = sheet
+  .filter((line) => {
+    return line[1] === "Normandie" || line[1] === "Pays de la Loire";
+  })
+  .map((line) => {
+    const newLine = line.splice(2, 3);
+    newLine.push(
+      `https://placedasile.gouv.fr/ajout-structure/${newLine[2]}/01-identification`
+    );
+    return newLine;
+  });
+
+const departements = [...new Set(structures.map((structure) => structure[0]))];
+
+departements.forEach((departement) => {
+  const structuresParDepartement = structures.filter((structure) => {
+    return structure[0] === departement;
+  });
+
+  const buffer = xlsx.build([
+    { name: "Structures", data: structuresParDepartement, options: {} },
+  ]);
+  fs.writeFile(`./scripts/structures-${departement}.xlsx`, buffer, (error) => {
+    console.error(error);
+  });
+});
