bcb5a4f8bf2fcf46bf7d9c910d147180d0f0999b
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-06-12 15:56:02 +0200

    :boom: Schemas refacto + fileupload bugfixx

--- src/app/(password-protected)/ajout-structure/[dnaCode]/04-documents/documents.ts
+++ src/app/(password-protected)/ajout-structure/[dnaCode]/04-documents/documents.ts
@@ -65,6 +65,6 @@ export const structureSubventionneesDocuments: StructureDocument[] = [
 export type StructureDocument = {
   label: string;
   subLabel?: string;
-  value: string;
+  value: FileUploadCategory;
   yearIndex: number;
 };
---
+++ src/app/(password-protected)/ajout-structure/validation/adressesSchema.ts
@@ -0,0 +1,75 @@
+import { Repartition } from "@/types/adresse.type";
+import z from "zod";
+
+const singleAdresseSchema = z.object({
+  adresseComplete: z.string().nonempty(),
+  adresse: z.string().nonempty(),
+  codePostal: z.string().nonempty(),
+  commune: z.string().nonempty(),
+  repartition: z.nativeEnum(Repartition),
+  places: z
+    .preprocess(
+      (val) => (val === "" ? undefined : Number(val)),
+      z.number().min(0)
+    )
+    .optional(),
+  typologies: z.array(z.any()).optional(),
+});
+
+const extendedAdresseSchema = singleAdresseSchema.extend({
+  repartition: z.nativeEnum(Repartition),
+  qpv: z.boolean().optional(),
+  logementSocial: z.boolean().optional(),
+});
+
+export type AdressesFormValues = z.infer<typeof AdressesSchema>;
+export const AdressesSchema = z
+  .object({
+    nom: z.string().optional(),
+    adresseAdministrativeComplete: z.string().min(3),
+    adresseAdministrative: z.string().nonempty(),
+    codePostalAdministratif: z.string().nonempty(),
+    communeAdministrative: z.string().nonempty(),
+    departementAdministratif: z.string().nonempty(),
+    typeBati: z.nativeEnum(Repartition),
+    sameAddress: z.boolean().optional(),
+    adresses: z.array(
+      extendedAdresseSchema.superRefine((adresse, ctx) => {
+        // Check if this address has a non-empty adresseComplete but empty places
+
+        if (
+          adresse.adresseComplete &&
+          adresse.adresseComplete.trim() !== "" &&
+          (adresse.places === undefined || adresse.places === null)
+        ) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Requis",
+            path: ["places"],
+          });
+        }
+      })
+    ),
+  })
+  .refine((data) => {
+    return data.adresses.some((adresse) => adresse.places !== undefined);
+  }, "Au moins une adresse doit avoir des places")
+  .superRefine((data, ctx) => {
+    if (data.typeBati !== Repartition.MIXTE) {
+      return;
+    }
+
+    // When typeBati is MIXTE, check each address and add specific errors
+    data.adresses.forEach((adresse, index) => {
+      if (
+        adresse.repartition !== Repartition.DIFFUS &&
+        adresse.repartition !== Repartition.COLLECTIF
+      ) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Type de bâti requis",
+          path: ["adresses", index, "repartition"],
+        });
+      }
+    });
+  });
