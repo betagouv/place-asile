35976d02c3075304d512970383a8d4a15f8cd112
Alezco <benjaminmorali4@hotmail.fr>
2025-06-12 11:27:14 +0200

    :chart_with_upwards_trend: Add Matomo analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import "../../build.css";
 import "./globals.css"; // IMPORTANT cet import doit rester en premier pour que les layers css soient respectés
 
 import type { Metadata } from "next";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import { Providers } from "./Providers";
 import {
   DsfrHead,
   getHtmlAttributes,
 } from "./dsfr-bootstrap/server-only-index";
 import { DsfrProvider } from "./dsfr-bootstrap";
+import { init } from "@socialgouv/matomo-next";
 
 export const metadata: Metadata = {
   title: "Place d'asile",
@@ -17,6 +18,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: PropsWithChildren) {
   console.log("Environnement :", process.env.NODE_ENV);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      init({
+        url: process.env.MATOMO_URL!,
+        siteId: process.env.MATOMO_SITE_ID!,
+        disableCookies: true,
+      });
+    }
+  }, []);
 
   const lang = "fr";
   return (
