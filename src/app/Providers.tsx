ea90457f5be2a824fe7d716719bb940e9903188c
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-06-12 16:03:02 +0200

    :bug: Fix Matomo use client (#259)

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,10 +1,21 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { PropsWithChildren, ReactElement } from "react";
+import { PropsWithChildren, ReactElement, useEffect } from "react";
 // Import the zodErrorMap to apply the global error map
 import "@/app/utils/zodErrorMap";
+import { init } from "@socialgouv/matomo-next";
 
 export const Providers = ({ children }: PropsWithChildren): ReactElement => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      init({
+        url: process.env.MATOMO_URL!,
+        siteId: process.env.MATOMO_SITE_ID!,
+        disableCookies: true,
+      });
+    }
+  }, []);
+
   return <SessionProvider>{children}</SessionProvider>;
 };
