599b3b85d5755889009c71ce1e60bd8decffbc20
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-06-13 19:06:21 +0200

    :art: Structure refacs

--- src/app/(authenticated)/structures/[id]/DefaultStructure.tsx
+++ src/app/(authenticated)/structures/[id]/DefaultStructure.tsx
@@ -1,6 +1,5 @@
 import { Structure } from "@/types/structure.type";
-import { ReactElement, Ref } from "react";
-import { StructureHeader } from "./(header)/StructureHeader";
+import { ReactElement } from "react";
 import { DefaultDescriptionBlock } from "./(description)/DefaultDescriptionBlock";
 import {
   getRepartition,
@@ -12,114 +11,81 @@ import { CalendrierBlock } from "./(calendrier)/CalendrierBlock";
 import { DefaultControlesBlock } from "./(controles)/DefaultControlesBlock";
 import { DefaultTypePlaceBlock } from "./(type-places)/DefaultTypePlaceBlock";
 import { NotesBlock } from "./(notes)/NotesBlock";
+import { Section } from "./components/Section";
 
-export const DefaultStructure = ({
-  structure,
-  structureHeaderHeight,
-  structureHeaderRef,
-}: Props): ReactElement => {
+export const DefaultStructure = ({ structure }: Props): ReactElement => {
+  // TODO : Refac props from blocks to remove the props and pass them from context
   return (
     <>
-      <StructureHeader
-        type={structure.type}
-        operateur={structure.operateur}
-        nbPlaces={structure.nbPlaces}
-        nom={structure.nom}
-        commune={structure.communeAdministrative}
-        departement={structure.departementAdministratif}
-        ref={structureHeaderRef}
-      />
-      <div className="bg-grey flex flex-col gap-2 p-4">
-        <section
-          style={{ scrollMarginTop: `${structureHeaderHeight}px` }}
-          id="description"
-        >
-          <DefaultDescriptionBlock
-            creationDate={structure.creationDate}
-            dnaCode={structure.dnaCode}
-            operateur={structure.operateur}
-            publicType={structure.public}
-            adresse={structure.adresseAdministrative}
-            nom={structure.nom}
-            codePostal={structure.codePostalAdministratif}
-            commune={structure.communeAdministrative}
-            repartition={getRepartition(structure)}
-            type={structure.type}
-            finessCode={structure.finessCode}
-            cpom={structure.cpom}
-            lgbt={structure.lgbt}
-            fvvTeh={structure.fvvTeh}
-            contacts={structure.contacts || []}
-            adresses={structure.adresses || []}
-          />
-        </section>
-        <section
-          style={{ scrollMarginTop: `${structureHeaderHeight}px` }}
-          id="calendrier"
-        >
-          <CalendrierBlock
-            debutPeriodeAutorisation={structure.debutPeriodeAutorisation}
-            finPeriodeAutorisation={structure.finPeriodeAutorisation}
-            debutConvention={structure.debutConvention}
-            finConvention={structure.finConvention}
-            debutCpom={structure.debutCpom}
-            finCpom={structure.finCpom}
-          />
-        </section>
-        <section
-          style={{ scrollMarginTop: `${structureHeaderHeight}px` }}
-          id="places"
-        >
-          <DefaultTypePlaceBlock
-            adresses={structure.adresses || []}
-            structureTypologies={structure.typologies || []}
-            placesAutorisees={structure.nbPlaces}
-            placesACreer={structure.placesACreer}
-            placesAFermer={structure.placesAFermer}
-            echeancePlacesACreer={structure.echeancePlacesACreer}
-            echeancePlacesAFermer={structure.echeancePlacesAFermer}
-            placesPmr={structure?.typologies?.[0]?.pmr || 0}
-            placesLgbt={structure?.typologies?.[0]?.lgbt || 0}
-            placesFvvTeh={structure?.typologies?.[0]?.fvvTeh || 0}
-            placesQPV={getCurrentPlacesQpv(structure)}
-            placesLogementsSociaux={getCurrentPlacesLogementsSociaux(structure)}
-          />
-        </section>
-        <section
-          style={{ scrollMarginTop: `${structureHeaderHeight}px` }}
-          id="controle"
-        >
-          <DefaultControlesBlock
-            evaluations={structure.evaluations || []}
-            controles={structure.controles || []}
-            evenementsIndesirablesGraves={
-              structure.evenementsIndesirablesGraves || []
-            }
-          />
-        </section>
-        <section
-          style={{ scrollMarginTop: `${structureHeaderHeight}px` }}
-          id="activites"
-        >
-          <ActivitesBlock
-            activites={structure.activites || []}
-            debutConvention={structure.debutConvention}
-            finConvention={structure.finConvention}
-          />
-        </section>
-        <section
-          style={{ scrollMarginTop: `${structureHeaderHeight}px` }}
-          id="notes"
-        >
-          <NotesBlock notes={structure.notes} />
-        </section>
-      </div>
+      <Section id="description">
+        <DefaultDescriptionBlock
+          creationDate={structure.creationDate}
+          dnaCode={structure.dnaCode}
+          operateur={structure.operateur}
+          publicType={structure.public}
+          adresse={structure.adresseAdministrative}
+          nom={structure.nom}
+          codePostal={structure.codePostalAdministratif}
+          commune={structure.communeAdministrative}
+          repartition={getRepartition(structure)}
+          type={structure.type}
+          finessCode={structure.finessCode}
+          cpom={structure.cpom}
+          lgbt={structure.lgbt}
+          fvvTeh={structure.fvvTeh}
+          contacts={structure.contacts || []}
+          adresses={structure.adresses || []}
+        />
+      </Section>
+      <Section id="calendrier">
+        <CalendrierBlock
+          debutPeriodeAutorisation={structure.debutPeriodeAutorisation}
+          finPeriodeAutorisation={structure.finPeriodeAutorisation}
+          debutConvention={structure.debutConvention}
+          finConvention={structure.finConvention}
+          debutCpom={structure.debutCpom}
+          finCpom={structure.finCpom}
+        />
+      </Section>
+      <Section id="places">
+        <DefaultTypePlaceBlock
+          adresses={structure.adresses || []}
+          structureTypologies={structure.typologies || []}
+          placesAutorisees={structure.nbPlaces}
+          placesACreer={structure.placesACreer}
+          placesAFermer={structure.placesAFermer}
+          echeancePlacesACreer={structure.echeancePlacesACreer}
+          echeancePlacesAFermer={structure.echeancePlacesAFermer}
+          placesPmr={structure?.typologies?.[0]?.pmr || 0}
+          placesLgbt={structure?.typologies?.[0]?.lgbt || 0}
+          placesFvvTeh={structure?.typologies?.[0]?.fvvTeh || 0}
+          placesQPV={getCurrentPlacesQpv(structure)}
+          placesLogementsSociaux={getCurrentPlacesLogementsSociaux(structure)}
+        />
+      </Section>
+      <Section id="controle">
+        <DefaultControlesBlock
+          evaluations={structure.evaluations || []}
+          controles={structure.controles || []}
+          evenementsIndesirablesGraves={
+            structure.evenementsIndesirablesGraves || []
+          }
+        />
+      </Section>
+      <Section id="activites">
+        <ActivitesBlock
+          activites={structure.activites || []}
+          debutConvention={structure.debutConvention}
+          finConvention={structure.finConvention}
+        />
+      </Section>
+      <Section id="notes">
+        <NotesBlock notes={structure.notes} />
+      </Section>
     </>
   );
 };
 
 type Props = {
   structure: Structure;
-  structureHeaderHeight: number;
-  structureHeaderRef: Ref<HTMLDivElement>;
 };
---
+++ src/app/(authenticated)/structures/[id]/finalisation/components/Steps.tsx
@@ -0,0 +1,27 @@
+import Stepper from "@codegouvfr/react-dsfr/Stepper";
+import React from "react";
+const steps = [
+  "Identification de la structure",
+  "Adresses",
+  "Types de places",
+  "Finances",
+  "Contrôle qualité & actes administratifs",
+  "Notes",
+];
+
+export default function Steps({ currentStep }: StepsProps) {
+  const title = steps[currentStep - 1];
+  const nextTitle = currentStep < steps.length ? steps[currentStep] : "";
+  return (
+    <Stepper
+      currentStep={currentStep}
+      nextTitle={nextTitle}
+      stepCount={steps.length}
+      title={title}
+    />
+  );
+}
+
+type StepsProps = {
+  currentStep: number;
+};
