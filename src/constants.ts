7fc2b312d1d8f43679a94061ebd9c42af5e1e3cc
Alezco <benjaminmorali4@hotmail.fr>
2025-07-01 11:43:42 +0200

    :wrench: Update upload limit to 70MB

--- src/constants.ts
+++ src/constants.ts
@@ -22,7 +22,7 @@ export const ALLOWED_MIME_TYPES = [
   ...SPREADSHEET_MIME_TYPES,
 ];
 
-export const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10 Mo
+export const MAX_FILE_SIZE = 70 * 1024 * 1024; // 70 Mo
 
 export const MODELE_DIFFUS_LINK = "/adresses-diffus.xlsx";
 export const MODELE_MIXTE_LINK = "/adresses-mixte.xlsx";
