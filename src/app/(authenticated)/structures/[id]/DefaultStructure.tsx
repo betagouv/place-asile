715696f155727161ed9251c6aa9ca53a146be935
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-07-03 12:26:56 +0200

    :wrench: ProConnect (#300)

--- src/app/(authenticated)/structures/[id]/DefaultStructure.tsx
+++ src/app/(authenticated)/structures/[id]/DefaultStructure.tsx
@@ -4,7 +4,7 @@ import { CalendrierBlock } from "./(calendrier)/CalendrierBlock";
 import { Section } from "./components/Section";
 import { DefaultTypePlaceBlock } from "./(type-places)/DefaultTypePlaceBlock";
 import { DefaultControlesBlock } from "./(controles)/DefaultControlesBlock";
-import { ActivitesBlock } from "./(activites)/ActivitesBlock";
+// import { ActivitesBlock } from "./(activites)/ActivitesBlock";
 import { NotesBlock } from "./(notes)/NotesBlock";
 import { FinancesBlock } from "./(finances)/FinancesBlock";
 
@@ -26,9 +26,9 @@ export const DefaultStructure = (): ReactElement => {
       <Section id="controle">
         <DefaultControlesBlock />
       </Section>
-      <Section id="activites">
+      {/* <Section id="activites">
         <ActivitesBlock />
-      </Section>
+      </Section> */}
       <Section id="notes">
         <NotesBlock />
       </Section>
