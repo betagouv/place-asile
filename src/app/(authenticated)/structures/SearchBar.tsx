b73ee5b8c9d708d8124434c88b4bcd181546d5c9
Benjamin Morali <benjaminmorali4@hotmail.fr>
2025-07-30 11:27:06 +0200

    :sparkles: Add searchbar (#452)

---
+++ src/app/(authenticated)/structures/SearchBar.tsx
@@ -0,0 +1,44 @@
+import { Structure } from "@/types/structure.type";
+import { ChangeEvent, ReactElement } from "react";
+
+export const SearchBar = ({
+  structures,
+  setFilteredStructures,
+}: Props): ReactElement => {
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = event.target.value.toLowerCase();
+    if (searchTerm.length > 0) {
+      const filteredStructures = structures.filter((structure) => {
+        const communes =
+          structure.adresses?.flatMap((adresse) =>
+            adresse.commune.toLowerCase()
+          ) || [];
+        const matchesDnaCode = structure.dnaCode
+          .toLowerCase()
+          .includes(searchTerm);
+        const matchesCommune = communes.some((commune) =>
+          commune.includes(searchTerm)
+        );
+        return matchesDnaCode || matchesCommune;
+      });
+      setFilteredStructures(filteredStructures);
+    }
+  };
+
+  return (
+    <div className="border border-disabled-grey h-8 flex items-center">
+      <span className="fr-icon-search-line fr-icon--sm text-label-blue-france px-2" />
+      <input
+        type="text"
+        placeholder="DNA ou commune"
+        id="search"
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+type Props = {
+  structures: Structure[];
+  setFilteredStructures: (filteredStructures: Structure[]) => void;
+};
