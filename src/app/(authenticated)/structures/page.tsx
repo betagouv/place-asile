0c4a1c10678830745f6443f9bde18fc66a6f4000
Alezco <benjaminmorali4@hotmail.fr>
2025-07-30 11:25:57 +0200

    :sparkles: Add searchbar

--- src/app/(authenticated)/structures/page.tsx
+++ src/app/(authenticated)/structures/page.tsx
@@ -6,6 +6,7 @@ import { SegmentedControl } from "../../components/common/SegmentedControl";
 import { useStructures } from "../../hooks/useStructures";
 import { StructuresTable } from "./StructuresTable";
 import { Structure } from "@/types/structure.type";
+import { SearchBar } from "./SearchBar";
 
 export default function Structures(): ReactElement {
   const [structures, setStructures] = useState<Structure[]>([]);
@@ -16,6 +17,7 @@ export default function Structures(): ReactElement {
     const loadStructures = async () => {
       const result = await getStructures();
       setStructures(result);
+      setFilteredStructures(result);
     };
     loadStructures();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -34,9 +36,11 @@ export default function Structures(): ReactElement {
     []
   );
 
+  const [filteredStructures, setFilteredStructures] = useState(structures);
+
   return (
     <div className="h-screen w-full flex flex-col">
-      <div className="flex justify-between fr-p-2w border-bottom min-h-[4.35rem] items-center">
+      <div className="flex fr-p-2w border-bottom min-h-[4.35rem] items-center">
         <SegmentedControl
           name="Visualisation"
           options={options}
@@ -49,16 +53,31 @@ export default function Structures(): ReactElement {
             Structures d’hébergement
           </h2>
         </SegmentedControl>
-        <p className="text-mention-grey mb-0">{structures.length} entrées</p>
+        <div className="grow" />
+        <SearchBar
+          structures={structures}
+          setFilteredStructures={setFilteredStructures}
+        />
+        <p className="pl-3 text-mention-grey mb-0">
+          {filteredStructures.length} entrée(s)
+        </p>
       </div>
       {selectedVisualization === "tableau" && (
-        <StructuresTable
-          structures={structures}
-          ariaLabelledBy="structures-titre"
-        />
+        <>
+          {filteredStructures.length > 0 ? (
+            <StructuresTable
+              structures={filteredStructures}
+              ariaLabelledBy="structures-titre"
+            />
+          ) : (
+            <p className="p-2">
+              Aucun résultat ne correspond à votre recherche
+            </p>
+          )}
+        </>
       )}
       {selectedVisualization === "carte" && (
-        <StructuresMap structures={structures} />
+        <StructuresMap structures={filteredStructures} />
       )}
     </div>
   );
