b192cfb6db45162230ca7e3eb282555395e425e5
Alezco <benjaminmorali4@hotmail.fr>
2025-08-26 17:29:35 +0200

    :lock: Improve ProConnect middleware

--- prisma/seeders/operateur.seed.ts
+++ prisma/seeders/operateur.seed.ts
@@ -2,6 +2,7 @@ import { Operateur } from "@prisma/client";
 import { fakerFR as faker } from "@faker-js/faker";
 
 export const createFakeOperateur = (): Omit<Operateur, "id"> => {
+  // TODO : refacto pour éviter les conflits de nom
   const operateurs = Array.from(
     { length: 5 },
     (_, index) => `Opérateur ${index + 1}`
