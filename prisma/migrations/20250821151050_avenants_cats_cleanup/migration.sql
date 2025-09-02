a5ece2d3ab50f16c13d698b9b68a188e53b487b5
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-09-02 16:14:19 +0200

    :rotating_light: Maybe fix missing migration

---
+++ prisma/migrations/20250821151050_avenants_cats_cleanup/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [ARRETE_AUTORISATION_AVENANT,CONVENTION_AVENANT,ARRETE_TARIFICATION_AVENANT,CPOM_AVENANT] on the enum `FileUploadCategory` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "FileUploadCategory_new" AS ENUM ('BUDGET_PREVISIONNEL_DEMANDE', 'RAPPORT_BUDGETAIRE', 'BUDGET_PREVISIONNEL_RETENU', 'BUDGET_RECTIFICATIF', 'COMPTE_ADMINISTRATIF_SOUMIS', 'RAPPORT_ACTIVITE', 'COMPTE_ADMINISTRATIF_RETENU', 'DEMANDE_SUBVENTION', 'COMPTE_RENDU_FINANCIER', 'RAPPORT_ACTIVITE_OPERATEUR', 'ARRETE_AUTORISATION', 'CONVENTION', 'ARRETE_TARIFICATION', 'CPOM', 'INSPECTION_CONTROLE', 'AUTRE');
+ALTER TABLE "FileUpload" ALTER COLUMN "category" TYPE "FileUploadCategory_new" USING ("category"::text::"FileUploadCategory_new");
+ALTER TYPE "FileUploadCategory" RENAME TO "FileUploadCategory_old";
+ALTER TYPE "FileUploadCategory_new" RENAME TO "FileUploadCategory";
+DROP TYPE "FileUploadCategory_old";
+COMMIT;
---
+++ prisma/migrations/20250902140821_missing_migration_fix/migration.sql
@@ -0,0 +1,12 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "public"."FileUploadCategory" ADD VALUE 'ARRETE_AUTORISATION_AVENANT';
+ALTER TYPE "public"."FileUploadCategory" ADD VALUE 'CONVENTION_AVENANT';
+ALTER TYPE "public"."FileUploadCategory" ADD VALUE 'ARRETE_TARIFICATION_AVENANT';
+ALTER TYPE "public"."FileUploadCategory" ADD VALUE 'CPOM_AVENANT';
