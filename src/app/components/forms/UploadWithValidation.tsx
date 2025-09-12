e8493e1f0b5e8aff58625977fd3875732e57bb56
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-09-12 10:17:54 +0200

    :lipstick: A few cosmetics adjutments on file uploads Closes : #393

--- src/app/components/forms/UploadWithValidation.tsx
+++ src/app/components/forms/UploadWithValidation.tsx
@@ -11,8 +11,8 @@ import {
 import Upload from "./Upload";
 
 export default function UploadWithValidation<
-  TFieldValues extends FieldValues = FieldValues
->({ name, control, id }: UploadWithValidationProps<TFieldValues>) {
+  TFieldValues extends FieldValues = FieldValues,
+>({ name, control, id, className }: UploadWithValidationProps<TFieldValues>) {
   const finalControl = control;
 
   const { field } = useController({
@@ -27,6 +27,7 @@ export default function UploadWithValidation<
       id={id}
       state={fieldState?.invalid ? "error" : field.value ? "success" : "idle"}
       errorMessage={fieldState?.error?.message}
+      className={className}
       {...field}
     />
   );
@@ -37,4 +38,5 @@ type UploadWithValidationProps<TFieldValues extends FieldValues = FieldValues> =
     name: Path<TFieldValues>;
     control?: Control<TFieldValues>;
     id?: string;
+    className?: string;
   };
