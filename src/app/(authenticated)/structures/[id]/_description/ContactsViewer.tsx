399649d479aca2b3754ce6c227a4d086968ac59b
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-09-15 10:31:13 +0200

    :lipstick: Handle adresse hebergement Closes : #331 (#544)
    * :lipstick: Handle adresse hebergement Closes : #331
    
    * :art: Collective Adress hook
    
    * :art: Collective adress from hook to utils
    
    * :art: Fix import err

--- src/app/(authenticated)/structures/[id]/_description/ContactsViewer.tsx
+++ src/app/(authenticated)/structures/[id]/_description/ContactsViewer.tsx
@@ -2,6 +2,7 @@
 
 import { ReactElement, useState } from "react";
 
+import { getCollectiveAddress } from "@/app/utils/collectiveAdress.util";
 import { formatPhoneNumber } from "@/app/utils/phone.util";
 
 import { useStructureContext } from "../context/StructureClientContext";
@@ -18,34 +19,51 @@ export const ContactsViewer = (): ReactElement => {
     contacts,
   } = structure || {};
 
+  const collectiveAddress = getCollectiveAddress(structure);
+
   return (
     <>
-      <strong className="fr-pr-2w">Adresse administrative</strong>
-      <span className="fr-pr-1w">
-        {nom ? `${nom}, ` : ""}
-        {adresseAdministrative}, {codePostalAdministratif}{" "}
-        {communeAdministrative}
-      </span>
-      <button
-        className={`fr-btn fr-btn--sm fr-btn--icon-left fr-btn--tertiary-no-outline ${
-          showContacts ? "fr-icon-eye-off-line" : "fr-icon-eye-line"
-        }`}
-        onClick={() => setShowContacts(!showContacts)}
-      >
-        {showContacts ? "Masquer les contacts" : "Voir les contacts"}
-      </button>
-      {showContacts && (
-        <div className="text-mention-grey">
-          {contacts?.map((contact) => (
-            <div key={contact.email} className="pb-1">
-              <strong className="fr-pr-3w">
-                {contact.prenom} {contact.nom} ({contact.role})
-              </strong>
-              <span className="fr-pr-3w">{contact.email}</span>
-              <span>{formatPhoneNumber(contact.telephone)}</span>
-            </div>
-          ))}
-        </div>
+      <div className="flex items-center">
+        <strong className="fr-pr-2w">Adresse administrative</strong>
+        <span className="fr-pr-1w">
+          {nom ? `${nom}, ` : ""}
+          {adresseAdministrative}, {codePostalAdministratif}{" "}
+          {communeAdministrative}
+        </span>
+        <button
+          className={`fr-btn fr-btn--sm fr-btn--icon-left fr-btn--tertiary-no-outline ${
+            showContacts ? "fr-icon-eye-off-line" : "fr-icon-eye-line"
+          }`}
+          onClick={() => setShowContacts(!showContacts)}
+        >
+          {showContacts ? "Masquer les contacts" : "Voir les contacts"}
+        </button>
+      </div>
+      <div>
+        {showContacts && (
+          <div className="text-mention-grey">
+            {contacts?.map((contact) => (
+              <div key={contact.email} className="pb-1">
+                <strong className="fr-pr-3w">
+                  {contact.prenom} {contact.nom} ({contact.role})
+                </strong>
+                <span className="fr-pr-3w">{contact.email}</span>
+                <span>{formatPhoneNumber(contact.telephone)}</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      {collectiveAddress && (
+        <>
+          <hr />
+          <div className="flex items-center">
+            <strong className="pr-2">Adresse d&apos;hébergement</strong>
+            {collectiveAddress.adresse}, {collectiveAddress.codePostal}{" "}
+            {collectiveAddress.commune}
+          </div>
+        </>
       )}
     </>
   );
---
+++ src/app/utils/collectiveAdress.util.ts
@@ -0,0 +1,19 @@
+import { Repartition } from "@/types/adresse.type";
+import { Structure } from "@/types/structure.type";
+
+export const getCollectiveAddress = (structure: Structure | null) => {
+  const collectiveAddress =
+    structure?.adresses &&
+    structure.adresses.length === 1 &&
+    structure.adresses[0].repartition.toUpperCase() ===
+      Repartition.COLLECTIF.toUpperCase()
+      ? structure.adresses[0]
+      : null;
+
+  if (!collectiveAddress) return null;
+
+  if (structure?.adresseAdministrative === collectiveAddress.adresse)
+    return null;
+
+  return collectiveAddress;
+};
