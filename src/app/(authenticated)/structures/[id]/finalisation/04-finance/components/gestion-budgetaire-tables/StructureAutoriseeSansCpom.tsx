a094a1b30c744c9680be17a81753bc7377d89c2d
Florian <hi@florianpanchout.com>
2025-09-24 10:23:06 +0200

    🔧 Update budget input fields to enable editing in StructureAutorisee and StructureAutoriseeSansCpom; adjust validation schema

--- src/app/(authenticated)/structures/[id]/finalisation/04-finance/components/gestion-budgetaire-tables/StructureAutoriseeSansCpom.tsx
+++ src/app/(authenticated)/structures/[id]/finalisation/04-finance/components/gestion-budgetaire-tables/StructureAutoriseeSansCpom.tsx
@@ -126,7 +126,6 @@ export const StructureAutoriseeSansCpom = () => {
                 label=""
                 className="mb-0 mx-auto items-center [&_p]:hidden [&_input]:w-full"
                 variant="simple"
-                disabled={index === 0}
               />
               &nbsp;€
             </div>
@@ -174,7 +173,7 @@ export const StructureAutoriseeSansCpom = () => {
                 label=""
                 className="mb-0 mx-auto items-center [&_p]:hidden [&_input]:w-full"
                 variant="simple"
-                disabled={[0, 1, 2].includes(index)}
+                disabled={[0, 1].includes(index)}
               />
               &nbsp;€
             </div>
@@ -189,7 +188,7 @@ export const StructureAutoriseeSansCpom = () => {
                 label=""
                 className="mb-0 mx-auto items-center [&_p]:hidden [&_input]:w-full"
                 variant="simple"
-                disabled={[0, 1, 2].includes(index)}
+                disabled={[0, 1].includes(index)}
               />
               &nbsp;€
             </div>
@@ -205,7 +204,7 @@ export const StructureAutoriseeSansCpom = () => {
                 label=""
                 className="mb-0 mx-auto items-center [&_p]:hidden [&_input]:w-full"
                 variant="simple"
-                disabled={[0, 1, 2].includes(index)}
+                disabled={[0, 1].includes(index)}
               />
               &nbsp;€
             </div>
