87a7c05ff70f18d5069f6e155cfa8a1b13ff90fe
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-09-30 12:28:22 +0200

    :construction: E2e starter code with agent form exemple

---
+++ playwright.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests/_e2e",
+  use: {
+    baseURL: "http://localhost:3000",
+    headless: false,
+    viewport: { width: 1280, height: 800 },
+    ignoreHTTPSErrors: true,
+    video: "retain-on-failure",
+    screenshot: "only-on-failure",
+  },
+});
