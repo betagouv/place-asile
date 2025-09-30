9712fb2ee8a09fa1a937c88b2eb1e1f9976f990a
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-09-30 11:39:46 +0200

    Jerem/572 invert steps merge (#596)
    * :twisted_rightwards_arrows: Now with merge dev
    
    * :test_tube: Fix test

--- tests/validation/finalisationFinanceSchema.test.ts
+++ tests/validation/finalisationFinanceSchema.test.ts
@@ -4,7 +4,7 @@ import {
   basicSchema,
   subventionneeAvecCpomSchema,
   subventionneeSchema,
-} from "@/app/(authenticated)/structures/[id]/finalisation/04-finance/validation/finalisationFinanceSchema";
+} from "@/app/(authenticated)/structures/[id]/finalisation/03-finance/validation/finalisationFinanceSchema";
 
 describe("finalisationFinanceSchema", () => {
   // Helper to create a valid budget base
