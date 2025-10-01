d5de41ccc1c3832ef150e122156d7d1d50bbda1c
Florian <hi@florianpanchout.com>
2025-10-01 18:27:56 +0200

    🔧 Remove react-number-format dependency and refactor InputSimple component to use standard input element.

--- src/app/components/ui/InputSimple.tsx
+++ src/app/components/ui/InputSimple.tsx
@@ -1,5 +1,4 @@
 import { HTMLAttributes, InputHTMLAttributes } from "react";
-import { NumericFormat } from "react-number-format";
 
 import { cn } from "@/app/utils/classname.util";
 
@@ -17,28 +16,10 @@ function InputSimple({
     state === "error" && "border-red-500"
   );
 
-  const input =
-    nativeInputProps?.type === "number" ? (
-      <NumericFormat
-        name={name}
-        {...nativeInputProps}
-        value={nativeInputProps?.value as string | number | undefined}
-        defaultValue={
-          nativeInputProps?.defaultValue as string | number | undefined
-        }
-        type={"text"}
-        thousandSeparator=" "
-        decimalSeparator=","
-        className={inputClassName}
-      />
-    ) : (
-      <input name={name} className={inputClassName} {...nativeInputProps} />
-    );
-
   return (
     <div className={cn("flex flex-col gap-2 text-sm", className)} {...props}>
       {label && <label htmlFor={nativeInputProps?.id}>{label}</label>}
-      {input}
+      <input name={name} className={inputClassName} {...nativeInputProps} />
       {state === "error" && (
         <p className="text-red-500 text-xs">{stateRelatedMessage}</p>
       )}
