cfce583f12fc5514d66cad145dd2f29832c639ef
Florian <hi@florianpanchout.com>
2025-10-07 18:57:59 +0200

    ✨ add calendrier edition

---
+++ src/app/(authenticated)/structures/[id]/modification/02-calendrier/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useStructureContext } from "@/app/(authenticated)/structures/[id]/context/StructureClientContext";
+import { FieldSetCalendrier } from "@/app/components/forms/fieldsets/structure/FieldSetCalendrier";
+import FormWrapper, {
+  FooterButtonType,
+} from "@/app/components/forms/FormWrapper";
+import { SubmitError } from "@/app/components/SubmitError";
+import { useAgentFormHandling } from "@/app/hooks/useAgentFormHandling";
+import { getDefaultValues } from "@/app/utils/defaultValues.util";
+
+import { ModificationTitle } from "../components/ModificationTitle";
+import { modificationCalendrierSchema } from "./validation/ModificationCalendrierSchema";
+
+export default function ModificationDescription() {
+  const { structure } = useStructureContext();
+
+  const { handleSubmit, state, backendError } = useAgentFormHandling({
+    nextRoute: `/structures/${structure.id}`,
+  });
+
+  const defaultValues = getDefaultValues({ structure });
+
+  return (
+    <>
+      <ModificationTitle
+        step="Calendrier"
+        closeLink={`/structures/${structure.id}`}
+      />
+      <FormWrapper
+        schema={modificationCalendrierSchema}
+        defaultValues={defaultValues}
+        onSubmit={handleSubmit}
+        mode="onChange"
+        resetRoute={`/structures/${structure.id}`}
+        submitButtonText="Valider"
+        availableFooterButtons={[
+          FooterButtonType.CANCEL,
+          FooterButtonType.SUBMIT,
+        ]}
+        className="border-[2px] border-solid border-[var(--text-title-blue-france)]"
+      >
+        <FieldSetCalendrier />
+      </FormWrapper>
+      {state === "error" && (
+        <SubmitError
+          structureDnaCode={structure.dnaCode}
+          backendError={backendError}
+        />
+      )}
+    </>
+  );
+}
---
+++ src/app/(authenticated)/structures/[id]/modification/02-calendrier/validation/ModificationCalendrierSchema.ts
@@ -0,0 +1,172 @@
+import z from "zod";
+
+import {
+  isStructureAutorisee,
+  isStructureSubventionnee,
+} from "@/app/utils/structure.util";
+import { createOptionalDateValidator } from "@/app/utils/zodCustomFields";
+import { StructureType } from "@/types/structure.type";
+
+export const modificationCalendrierSchema = z
+  .object({
+    dnaCode: z.string().nonempty(),
+    type: z.preprocess(
+      (val) => (val === "" ? undefined : val),
+      z.nativeEnum(StructureType)
+    ),
+    cpom: z.boolean(),
+    debutPeriodeAutorisation: createOptionalDateValidator(),
+    finPeriodeAutorisation: createOptionalDateValidator(),
+    debutConvention: createOptionalDateValidator(),
+    finConvention: createOptionalDateValidator(),
+    debutCpom: createOptionalDateValidator(),
+    finCpom: createOptionalDateValidator(),
+  })
+  .refine(
+    (data) => {
+      if (data.cpom && !data.debutCpom) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: "La date de début CPOM est obligatoire",
+      path: ["debutCpom"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.cpom && !data.finCpom) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: "La date de fin CPOM est obligatoire",
+      path: ["finCpom"],
+    }
+  )
+  .superRefine((data, ctx) => {
+    if (isStructureAutorisee(data.type)) {
+      if (!data.debutPeriodeAutorisation) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "La date de début de période d'autorisation est obligatoire pour les structures autorisées",
+          path: ["debutPeriodeAutorisation"],
+        });
+      }
+
+      if (data.cpom && !data.finPeriodeAutorisation) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "La date de fin de période d'autorisation est obligatoire pour les structures autorisées",
+          path: ["finPeriodeAutorisation"],
+        });
+      }
+    }
+  })
+  .refine(
+    (data) => {
+      if (
+        isStructureSubventionnee(data.type) &&
+        (!data.debutConvention || data.debutConvention === "")
+      ) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message:
+        "La date de début de convention est obligatoire pour les structures subventionnées",
+      path: ["debutConvention"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (
+        isStructureSubventionnee(data.type) &&
+        (!data.finConvention || data.finConvention === "")
+      ) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message:
+        "La date de fin de convention est obligatoire pour les structures subventionnées",
+      path: ["finConvention"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (
+        data.debutPeriodeAutorisation &&
+        data.finPeriodeAutorisation &&
+        data.debutPeriodeAutorisation !== "" &&
+        data.finPeriodeAutorisation !== ""
+      ) {
+        const debutDate = new Date(
+          data.debutPeriodeAutorisation.split("/").reverse().join("-")
+        );
+        const finDate = new Date(
+          data.finPeriodeAutorisation.split("/").reverse().join("-")
+        );
+        return finDate > debutDate;
+      }
+      return true;
+    },
+    {
+      message: "La date de fin doit être postérieure à la date de début",
+      path: ["finPeriodeAutorisation"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (
+        data.debutConvention &&
+        data.finConvention &&
+        data.debutConvention !== "" &&
+        data.finConvention !== ""
+      ) {
+        const debutDate = new Date(
+          data.debutConvention.split("/").reverse().join("-")
+        );
+        const finDate = new Date(
+          data.finConvention.split("/").reverse().join("-")
+        );
+        return finDate > debutDate;
+      }
+      return true;
+    },
+    {
+      message: "La date de fin doit être postérieure à la date de début",
+      path: ["finConvention"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (
+        data.debutCpom &&
+        data.finCpom &&
+        data.debutCpom !== "" &&
+        data.finCpom !== ""
+      ) {
+        const debutDate = new Date(
+          data.debutCpom.split("/").reverse().join("-")
+        );
+        const finDate = new Date(data.finCpom.split("/").reverse().join("-"));
+        return finDate > debutDate;
+      }
+      return true;
+    },
+    {
+      message: "La date de fin doit être postérieure à la date de début",
+      path: ["finCpom"],
+    }
+  );
+
+export type ModificationCalendrierFormValues = z.infer<
+  typeof modificationCalendrierSchema
+>;
