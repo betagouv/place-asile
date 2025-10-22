1e58d2dd091287f0eec0b24d86c4d7762157ae5a
Florian <hi@florianpanchout.com>
2025-10-22 08:13:28 +0200

    ✨ Add tags verifie and complete

--- src/app/(authenticated)/structures/[id]/finalisation/_components/Tab.tsx
+++ src/app/(authenticated)/structures/[id]/finalisation/_components/Tab.tsx
@@ -4,7 +4,13 @@ import { ReactNode } from "react";
 
 import { cn } from "@/app/utils/classname.util";
 
+import { useStructureContext } from "../../_context/StructureClientContext";
+
 export const Tab = ({ title, route, current, type }: Props) => {
+  const { structure } = useStructureContext();
+  const isCompleted = structure.finalisationSteps?.some(
+    (step) => step.label === route
+  );
   return (
     <Link
       href={route}
@@ -19,24 +25,43 @@ export const Tab = ({ title, route, current, type }: Props) => {
         <strong>{title}</strong>
         <span className="fr-icon-arrow-right-line"></span>
       </span>
-      {type === "verification" && (
-        <Tag
-          className="!bg-[var(--color-background-contrast-yellow-tournesol)] !text-[var(--color-text-action-high-yellow-tournesol)] font-bold"
-          iconId="fr-icon-eye-line"
-          small
-        >
-          À VÉRIFIER
-        </Tag>
-      )}
-      {type === "completion" && (
-        <Tag
-          className="!bg-[var(--color-background-contrast-warning)]  !text-[var(--color-text-default-warning)] font-bold"
-          iconId="fr-icon-eye-line"
-          small
-        >
-          A COMPLÈTER
-        </Tag>
-      )}
+      {type === "verification" &&
+        (isCompleted ? (
+          <Tag
+            className="!bg-[var(--success-950-100)] !text-[var(---success-425-625)] font-bold"
+            iconId="fr-icon-check-line"
+            small
+          >
+            VÉRIFIÉ
+          </Tag>
+        ) : (
+          <Tag
+            className="!bg-[var(--color-background-contrast-yellow-tournesol)] !text-[var(--color-text-action-high-yellow-tournesol)] font-bold"
+            iconId="fr-icon-eye-line"
+            small
+          >
+            À VÉRIFIER
+          </Tag>
+        ))}
+
+      {type === "completion" &&
+        (isCompleted ? (
+          <Tag
+            className="!bg-[var(--success-950-100)] !text-[var(---success-425-625)] font-bold"
+            iconId="fr-icon-check-line"
+            small
+          >
+            COMPLÉTÉ
+          </Tag>
+        ) : (
+          <Tag
+            className="!bg-[var(--color-background-contrast-warning)]  !text-[var(--color-text-default-warning)] font-bold"
+            iconId="fr-icon-eye-line"
+            small
+          >
+            A COMPLÈTER
+          </Tag>
+        ))}
     </Link>
   );
 };
