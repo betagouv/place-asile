faae2e93771c4c77e7c037571a46e32ec16566ef
BmnQuentin <quentinbmn@gmail.com>
2025-10-24 16:58:12 +0200

    fix one off scripts

--- prisma/utils/run-one-off.ts
+++ prisma/utils/run-one-off.ts
@@ -1,23 +1,12 @@
 import { execSync } from "child_process";
 
-const args = process.argv.slice(2);
-if (args.length === 0) {
-    console.error("❌ Merci de préciser le nom du script à exécuter.");
-    console.error("👉 Exemple : yarn one-off migrate-forms-prod");
+const arg = process.argv[2];
+if (!arg) {
+    console.error("❌ Erreur : tu dois fournir le nom du script, ex : yarn migrate 20251020-migrate-forms-and-steps");
     process.exit(1);
 }
 
-const scriptName = args[0];
-const scriptPath = `prisma/one-off-scripts/${scriptName}.ts`;
+const cmd = `yarn install --production=false && npx prisma generate && npx tsx prisma/one-off-scripts/${arg}.ts`;
 
-console.log(`🚀 Exécution du script one-off : ${scriptPath}`);
-
-try {
-    execSync(`yarn tsx prisma/one-off-scripts/${scriptName}.ts`, {
-        stdio: 'inherit',
-        cwd: process.cwd(),
-    });
-} catch (err) {
-    console.error(`❌ Erreur lors de l'exécution du script ${scriptName}`, err);
-    process.exit(1);
-}
+console.log(`🚀 Exécution de : ${cmd}\n`);
+execSync(cmd, { stdio: "inherit" });
