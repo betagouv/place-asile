37d88eb0494096fd13ca68a07b42c4956f55e789
BmnQuentin <quentinbmn@gmail.com>
2025-10-27 10:13:49 +0100

    fix architecture for one off scripts

---
+++ scripts/run-one-off.sh
@@ -0,0 +1,19 @@
+# Bash pour exécuter un script one off (par exemple post migration db pour créer des équivalences entre old et new field)
+
+set -e
+
+SCRIPT_NAME=$1
+
+if [ -z "$SCRIPT_NAME" ]; then
+  echo "❌ Erreur : merci de fournir le nom du script. Exemple : yarn one-off 20251020-migrate-forms-and-steps"
+  exit 1
+fi
+
+echo "🚀 Installation des dépendances"
+yarn install --production=false
+
+echo "⚙️ Génération Prisma..."
+npx prisma generate
+
+echo "🏃 Exécution du script ${SCRIPT_NAME}.ts"
+npx tsx scripts/one-off-scripts/${SCRIPT_NAME}.ts
