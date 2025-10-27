8d1a0df3c1566fc687912dae7aad0e841bb90f78
BmnQuentin <quentinbmn@gmail.com>
2025-10-27 14:09:52 +0100

    add departments and regions

--- scripts/views/001_structures_aggregate.sql
+++ scripts/views/001_structures_aggregate.sql
@@ -1,64 +1,54 @@
 -- Objective: aggregate indicators per structure
-CREATE OR REPLACE VIEW:"SCHEMA"."structures_aggregates" AS
-WITH -- Last typology by structure
+CREATE OR REPLACE VIEW :"SCHEMA"."structures_aggregates" AS WITH -- Last typology by structure
   structure_typologie_dernier_millesime AS (
-    SELECT DISTINCT
-      ON (st."structureDnaCode") st."structureDnaCode",
+    SELECT DISTINCT ON (st."structureDnaCode") st."structureDnaCode",
       st."placesAutorisees",
       st."pmr",
       st."lgbt",
       st."fvvTeh",
       st."date"
-    FROM
-      public."StructureTypologie" st
-    ORDER BY
-      st."structureDnaCode",
+    FROM public."StructureTypologie" st
+    ORDER BY st."structureDnaCode",
       st."date" DESC
   ),
   -- Last typology by address
   adresse_typologie_dernier_millesime AS (
-    SELECT DISTINCT
-      ON (aty."adresseId") a."structureDnaCode",
+    SELECT DISTINCT ON (aty."adresseId") a."structureDnaCode",
       a."id" AS "adresse_id",
       aty."placesAutorisees",
       aty."qpv",
       aty."logementSocial",
       aty."date"
-    FROM
-      public."AdresseTypologie" aty
+    FROM public."AdresseTypologie" aty
       JOIN public."Adresse" a ON a."id" = aty."adresseId"
-    ORDER BY
-      aty."adresseId",
+    ORDER BY aty."adresseId",
       aty."date" DESC
   ),
   -- Last activite by structure
   activite_dernier_millesime AS (
-    SELECT DISTINCT
-      ON (sa."structureDnaCode") sa."structureDnaCode",
+    SELECT DISTINCT ON (sa."structureDnaCode") sa."structureDnaCode",
       sa."date",
       sa."nbPlaces"
-    FROM
-      public."Activite" sa
-    ORDER BY
-      sa."structureDnaCode",
+    FROM public."Activite" sa
+    ORDER BY sa."structureDnaCode",
       sa."date" DESC
   ),
   -- Aggregate by structure on the last typologies of addresses
   adresses_agregees AS (
-    SELECT
-      adm."structureDnaCode",
+    SELECT adm."structureDnaCode",
       COUNT(DISTINCT adm."adresse_id") AS nb_adresses,
       SUM(adm."placesAutorisees") AS places_autorisees_adresse,
       SUM(adm."qpv") AS qpv_adresse,
       SUM(adm."logementSocial") AS logement_social_adresse,
       MAX(adm."date") AS date_adresse
-    FROM
-      adresse_typologie_dernier_millesime adm
-    GROUP BY
-      adm."structureDnaCode"
+    FROM adresse_typologie_dernier_millesime adm
+    GROUP BY adm."structureDnaCode"
   )
-SELECT
-  s."dnaCode" AS "dnaCode",
+SELECT s."dnaCode" AS "dnaCode",
+  s.latitude AS "latitude",
+  s.longitude AS "longitude",
+  s.public AS "public",
+  s.type AS "type",
   sdm."placesAutorisees" AS "places_autorisees_structure",
   sdm."pmr" AS "pmr_structure",
   sdm."lgbt" AS "lgbt_structure",
@@ -88,8 +78,7 @@ SELECT
   ) AS "pct_diff_places_activite",
   s."createdAt" AS "created_at",
   s."updatedAt" AS "updated_at"
-FROM
-  public."Structure" s
+FROM public."Structure" s
   LEFT JOIN structure_typologie_dernier_millesime sdm ON sdm."structureDnaCode" = s."dnaCode"
   LEFT JOIN adresses_agregees aa ON aa."structureDnaCode" = s."dnaCode"
   LEFT JOIN activite_dernier_millesime adm ON adm."structureDnaCode" = s."dnaCode"
