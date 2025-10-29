d18dcae84d19c32ac409d93f5531a1aa4b9feb84
Florian <hi@florianpanchout.com>
2025-10-29 09:25:09 +0100

    🐛 Fix "autre" and avenants validation and refactor schema

--- src/schemas/forms/base/acteAdministratif.schema.ts
+++ src/schemas/forms/base/acteAdministratif.schema.ts
@@ -1,36 +1,50 @@
 import z from "zod";
 
-import {
-  frenchDateToISO,
-  optionalFrenchDateToISO,
-} from "@/app/utils/zodCustomFields";
+import { optionalFrenchDateToISO } from "@/app/utils/zodCustomFields";
 import { ActeAdministratifCategory } from "@/types/file-upload.type";
 
-const avenantSchema = z.object({
-  key: z.string().optional(),
-  date: optionalFrenchDateToISO(),
-  category: z.enum(ActeAdministratifCategory),
-});
+const acteAdministratifSchema = z
+  .object({
+    key: z.string(),
+    date: optionalFrenchDateToISO(),
+    category: z.enum(ActeAdministratifCategory),
+    startDate: optionalFrenchDateToISO(),
+    endDate: optionalFrenchDateToISO(),
+    categoryName: z.string().nullish(),
+    parentFileUploadId: z.any().optional(),
+    uuid: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      if (data.category !== "AUTRE" && !data.parentFileUploadId) {
+        return !!data.startDate && !!data.endDate;
+      }
+      return true;
+    },
+    {
+      message: "Les dates de début et de fin sont obligatoires.",
+      path: ["endDate", "startDate"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.parentFileUploadId) {
+        return !!data.date;
+      }
+      return true;
+    },
+    {
+      message: "La date est obligatoire pour les avenants.",
+      path: ["date"],
+    }
+  );
 
-export const acteAdministratifSchema = z.object({
-  key: z.string(),
-  date: optionalFrenchDateToISO(),
-  category: z.enum(ActeAdministratifCategory),
-  startDate: frenchDateToISO(),
-  endDate: frenchDateToISO(),
-  avenants: z.array(avenantSchema).optional(),
-  categoryName: z.string().nullish(),
-  parentFileUploadId: z.any().optional(),
-  uuid: z.string().optional(),
-});
-
-export const acteAdministratifAutoSaveSchema = z.object({
+const acteAdministratifAutoSaveSchema = z.object({
   key: z.string().optional(),
   date: optionalFrenchDateToISO(),
   category: z.enum(ActeAdministratifCategory),
   startDate: optionalFrenchDateToISO(),
   endDate: optionalFrenchDateToISO(),
-  avenants: z.array(avenantSchema).optional(),
   categoryName: z.string().nullish(),
   parentFileUploadId: z.any().optional(),
   uuid: z.string().optional(),
