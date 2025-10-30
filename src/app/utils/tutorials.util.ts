a60311be42916c9ce30a9814917a0259c7a2dceb
Alezco <benjaminmorali4@hotmail.fr>
2025-10-30 10:53:45 +0100

    :sparkles: Update tutorial links

--- src/app/utils/tutorials.util.ts
+++ src/app/utils/tutorials.util.ts
@@ -8,16 +8,16 @@ export const getFinanceFormTutorialLink = ({
   hasCpom: boolean;
 }): string => {
   if (isAutorisee && hasCpom) {
-    return "https://www.loom.com/share/e27a0e312d3c4d0d9aa609970ae5a7f4?sid=4bfe4fbd-32de-4560-b7e2-6ab576cadaaf";
+    return "https://www.loom.com/share/e10b99446da747f9881c84e8c80cae4e";
   }
   if (isAutorisee && !hasCpom) {
-    return "https://www.loom.com/share/c5f156d305404effb7e6b33b82afb7eb?sid=a4939232-0a86-466a-9c0c-44d32c1b8b80";
+    return "https://www.loom.com/share/32683d9948084be9a24351a6b712f8d0";
   }
   if (isSubventionnee && hasCpom) {
-    return "https://www.loom.com/share/afb74bbadc604e48ab64c854f41223aa?sid=8188601d-3ba8-40d7-b6f2-3dca761d831b";
+    return "https://www.loom.com/share/dcf47a9164304db0a972a75cd5cf23f1";
   }
   if (isSubventionnee && !hasCpom) {
-    return "https://www.loom.com/share/558e836ecbfe45cc9afaedd6851ca5c4?sid=f00d8670-f7ee-43b8-ae4c-5269ae857ba2";
+    return "https://www.loom.com/share/c7f7b6b974f44d728790d9486b663010";
   }
   return "";
 };
