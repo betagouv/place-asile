d600c433bf3e6a3456da57e9558368e5aea39ba1
Florian <hi@florianpanchout.com>
2025-10-30 08:49:28 +0100

    ✨ Fix typo

--- src/app/(authenticated)/structures/[id]/finalisation/_components/Tab.tsx
+++ src/app/(authenticated)/structures/[id]/finalisation/_components/Tab.tsx
@@ -51,7 +51,7 @@ export const Tab = ({ title, route, current, type }: Props) => {
           iconId="fr-icon-eye-line"
           small
         >
-          A COMPLÈTER
+          À COMPLÈTER
         </Tag>
       )}
     </Link>
