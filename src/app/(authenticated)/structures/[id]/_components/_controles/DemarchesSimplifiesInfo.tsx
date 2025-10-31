008e2082654a670c10a1c5e7cfec2d691e59184e
Alezco <benjaminmorali4@hotmail.fr>
2025-10-31 14:38:03 +0100

    :sparkles: Update EIG notice

--- src/app/(authenticated)/structures/[id]/_components/_controles/DemarchesSimplifiesInfo.tsx
+++ src/app/(authenticated)/structures/[id]/_components/_controles/DemarchesSimplifiesInfo.tsx
@@ -4,7 +4,10 @@ import { ReactElement } from "react";
 export const DemarchesSimplifieesInfo = (): ReactElement => {
   return (
     <span className="text-sm m-3">
-      Pour connaître le détail d’un EIG, consultez le sur{" "}
+      Actuellement, seuls les EIG renseignés sur Démarches Simplifiées sont
+      affichés, l’ancienneté de cet historique dépend donc de la date à laquelle
+      votre région a été articulée avec l’outil. Pour connaître le détail d’un
+      EIG, consultez le sur{" "}
       <Link
         href="https://www.demarches-simplifiees.fr/"
         target="_blank"
