c5107b79a15acfc5eb1d0ba5c5d19fc8985b6f9e
Florian <hi@florianpanchout.com>
2025-11-12 12:04:28 +0100

    🐛 Fix Matomo init error on build

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,33 +2,21 @@
 
 import "@/app/utils/zodErrorMap";
 
-import { trackAppRouter } from "@socialgouv/matomo-next";
-import { usePathname, useSearchParams } from "next/navigation";
 import { SessionProvider } from "next-auth/react";
-import { PropsWithChildren, ReactElement, useEffect } from "react";
+import { PropsWithChildren, ReactElement, Suspense } from "react";
 
+import { Tracking } from "./components/Tracking";
 import { FetchStateProvider } from "./context/FetchStateContext";
 
 export const Providers = ({ children }: PropsWithChildren): ReactElement => {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      trackAppRouter({
-        url: process.env.NEXT_PUBLIC_MATOMO_URL!,
-        siteId: process.env.NEXT_PUBLIC_MATOMO_SITE_ID!,
-        pathname,
-        searchParams,
-        disableCookies: true,
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <SessionProvider>
-      <FetchStateProvider>{children}</FetchStateProvider>
+      <FetchStateProvider>
+        <Suspense fallback={null}>
+          <Tracking />
+        </Suspense>
+        {children}
+      </FetchStateProvider>
     </SessionProvider>
   );
 };
