60187de383b6014bc21b2d823aefc3b828b628d7
Florian <hi@florianpanchout.com>
2025-12-05 09:58:50 +0100

    ✨ Update documents financiers UI for modification form

--- src/app/components/forms/finance/documents/DocumentsFinanciers.tsx
+++ src/app/components/forms/finance/documents/DocumentsFinanciers.tsx
@@ -1,42 +1,67 @@
-import { fr } from "@codegouvfr/react-dsfr";
-import { ReactElement } from "react";
-import { Control } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 
+import { useStructureContext } from "@/app/(authenticated)/structures/[id]/_context/StructureClientContext";
+import { MaxSizeNotice } from "@/app/components/forms/MaxSizeNotice";
+import { getYearRange } from "@/app/utils/date.util";
 import {
-  structureAutoriseesDocuments,
-  structureSubventionneesDocuments,
-} from "@/app/components/forms/finance/documents/documentsStructures";
+  isStructureAutorisee,
+  isStructureSubventionnee,
+} from "@/app/utils/structure.util";
 import { DocumentsFinanciersFlexibleFormValues } from "@/schemas/forms/base/documentFinancier.schema";
 
-import { DocumentsFinanciersCategory } from "./DocumentsFinanciersCategory";
+import { FieldSetYearlyDocumentsFinanciers } from "../../fieldsets/structure/FieldSetYearlyDocumentsFinanciers";
+import { DocumentsFinanciersAccordion } from "./DocumentsFinanciersAccordion";
 
 export const DocumentsFinanciers = ({
-  isAutorisee,
-  control,
-  year,
-}: Props): ReactElement => {
-  const documentTypes = isAutorisee
-    ? structureAutoriseesDocuments
-    : structureSubventionneesDocuments;
+  hasAccordion,
+  className,
+}: {
+  hasAccordion?: boolean;
+  className?: string;
+}) => {
+  const { structure } = useStructureContext();
+  const { control } = useFormContext<DocumentsFinanciersFlexibleFormValues>();
+  const isSubventionnee = isStructureSubventionnee(structure?.type);
+  const isAutorisee = isStructureAutorisee(structure?.type);
+
+  const startYear = structure?.date303
+    ? new Date(structure.date303).getFullYear()
+    : new Date(structure.creationDate).getFullYear();
+
+  const { years } = getYearRange();
+
+  const yearsToDisplay = isSubventionnee ? years.slice(2) : years;
+
+  const noYear =
+    yearsToDisplay.filter((year) => Number(year) >= startYear).length === 0;
 
   return (
-    <div>
-      <div className={fr.cx("fr-accordions-group")}>
-        {documentTypes.map((documentType) => (
-          <DocumentsFinanciersCategory
-            documentType={documentType}
-            key={documentType.value}
-            control={control}
+    <div className={className}>
+      <MaxSizeNotice />
+      {noYear && (
+        <p className="text-disabled-grey mb-0 text-sm col-span-3">
+          La structure est trop récente et n’est pas en mesure de fournir de
+          documents. Vous pouvez valider cette étape.
+        </p>
+      )}
+      {yearsToDisplay.map((year, index) => (
+        <DocumentsFinanciersAccordion
+          key={year}
+          year={year}
+          startYear={startYear}
+          hasAccordion={hasAccordion}
+        >
+          <FieldSetYearlyDocumentsFinanciers
+            key={year}
             year={year}
+            startYear={startYear}
+            isAutorisee={isAutorisee}
+            control={control}
+            index={index}
+            hasAccordion={hasAccordion}
           />
-        ))}
-      </div>
+        </DocumentsFinanciersAccordion>
+      ))}
     </div>
   );
-};
-
-type Props = {
-  isAutorisee: boolean;
-  control: Control<DocumentsFinanciersFlexibleFormValues>;
-  year: number;
 };
---
+++ src/app/components/forms/finance/documents/DocumentsFinanciersList.tsx
@@ -0,0 +1,42 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import { ReactElement } from "react";
+import { Control } from "react-hook-form";
+
+import {
+  structureAutoriseesDocuments,
+  structureSubventionneesDocuments,
+} from "@/app/components/forms/finance/documents/documentsStructures";
+import { DocumentsFinanciersFlexibleFormValues } from "@/schemas/forms/base/documentFinancier.schema";
+
+import { DocumentsFinanciersCategory } from "./DocumentsFinanciersCategory";
+
+export const DocumentsFinanciersList = ({
+  isAutorisee,
+  control,
+  year,
+}: Props): ReactElement => {
+  const documentTypes = isAutorisee
+    ? structureAutoriseesDocuments
+    : structureSubventionneesDocuments;
+
+  return (
+    <div>
+      <div className={fr.cx("fr-accordions-group")}>
+        {documentTypes.map((documentType) => (
+          <DocumentsFinanciersCategory
+            documentType={documentType}
+            key={documentType.value}
+            control={control}
+            year={year}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+type Props = {
+  isAutorisee: boolean;
+  control: Control<DocumentsFinanciersFlexibleFormValues>;
+  year: number;
+};
