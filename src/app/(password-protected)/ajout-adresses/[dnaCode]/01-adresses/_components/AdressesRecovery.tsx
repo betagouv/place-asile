ddef38058a8c5eecdcbedee96ca6b0a688d738c7
Florian <hi@florianpanchout.com>
2025-12-20 15:00:53 +0100

    ✨ Enhance AdressesRecovery component with error handling and navigation on successful submission

--- src/app/(password-protected)/ajout-adresses/[dnaCode]/01-adresses/_components/AdressesRecovery.tsx
+++ src/app/(password-protected)/ajout-adresses/[dnaCode]/01-adresses/_components/AdressesRecovery.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useMemo } from "react";
+import { useRouter } from "next/navigation";
+import { useMemo, useState } from "react";
 
 import { FieldSetAdresseAdministrative } from "@/app/components/forms/fieldsets/structure/FieldSetAdresseAdministrative";
 import { FieldSetHebergement } from "@/app/components/forms/fieldsets/structure/FieldSetHebergement";
@@ -10,6 +11,7 @@ import FormWrapper, {
 import { useLocalStorage } from "@/app/hooks/useLocalStorage";
 import { useStructures } from "@/app/hooks/useStructures";
 import { transformFormAdressesToApiAdresses } from "@/app/utils/adresse.util";
+import { PLACE_ASILE_CONTACT_EMAIL } from "@/constants";
 import {
   AjoutAdressesFormValues,
   ajoutAdressesSchema,
@@ -19,6 +21,10 @@ import { FormKind } from "@/types/global";
 import { AdressesRecoveryModal } from "./AdressesRecoveryModal";
 
 export const AdressesRecovery = ({ dnaCode }: { dnaCode: string }) => {
+  const [state, setState] = useState<"idle" | "loading" | "error">("idle");
+  const [backendError, setBackendError] = useState("");
+
+  const router = useRouter();
   const { updateStructure } = useStructures();
   const { currentValue: localStorageValues } = useLocalStorage(
     `ajout-structure-${dnaCode}-adresses`,
@@ -30,7 +36,7 @@ export const AdressesRecovery = ({ dnaCode }: { dnaCode: string }) => {
   }, [localStorageValues]);
 
   const handleSubmit = async (data: AjoutAdressesFormValues) => {
-    await updateStructure({
+    const result = await updateStructure({
       dnaCode,
       adresses: transformFormAdressesToApiAdresses(data.adresses, dnaCode),
       typeBati: data.typeBati,
@@ -39,6 +45,19 @@ export const AdressesRecovery = ({ dnaCode }: { dnaCode: string }) => {
       communeAdministrative: data.communeAdministrative,
       departementAdministratif: data.departementAdministratif,
     });
+
+    if (result === "OK") {
+      router.push(`/ajout-adresses/${dnaCode}/02-confirmation`);
+    } else {
+      setBackendError(result);
+      setState("error");
+    }
+  };
+
+  const getErrorEmail = (error: string): string => {
+    const subject = "Problème avec le formulaire de Place d'asile";
+    const body = `Bonjour,%0D%0A%0D%0AAjoutez ici des informations supplémentaires...%0D%0A%0D%0ARapport d'erreur: ${error}`;
+    return `mailto:${PLACE_ASILE_CONTACT_EMAIL}?subject=${subject}&body=${body}`;
   };
 
   return (
@@ -56,6 +75,23 @@ export const AdressesRecovery = ({ dnaCode }: { dnaCode: string }) => {
         <hr />
         <FieldSetHebergement />
       </FormWrapper>
+      {state === "error" && (
+        <div className="flex items-end flex-col">
+          <p className="text-default-error m-0">
+            Une erreur s’est produite. Vos données restent sauvegardées dans le
+            navigateur.
+          </p>
+          <p className="text-default-error">
+            <a
+              href={getErrorEmail(backendError)}
+              className="underline"
+              target="_blank"
+            >
+              Nous prévenir
+            </a>
+          </p>
+        </div>
+      )}
       <AdressesRecoveryModal numAdressesRecovered={numAdressesRecovered} />
     </>
   );
