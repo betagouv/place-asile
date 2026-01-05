b7f90757e048e660ba052a868ec617daccd9c236
Florian <hi@florianpanchout.com>
2026-01-05 09:50:08 +0100

    ✨ Refactor AdressesRecoveryTypeBati component: wrap SelectWithValidation in a fieldset for improved accessibility and layout, and update the legend for better clarity.

--- src/app/(password-protected)/ajout-adresses/[dnaCode]/01-adresses/_components/AdressesRecoveryTypeBati.tsx
+++ src/app/(password-protected)/ajout-adresses/[dnaCode]/01-adresses/_components/AdressesRecoveryTypeBati.tsx
@@ -6,20 +6,25 @@ import { Repartition } from "@/types/adresse.type";
 export const AdressesRecoveryTypeBati = () => {
   const { control } = useFormContext();
   return (
-    <SelectWithValidation
-      name="typeBati"
-      control={control}
-      label="Type de bâti"
-      id="typeBati"
-      className="max-w-xs"
-    >
-      <option value="">Sélectionnez une option</option>
+    <fieldset className="flex flex-col gap-6">
+      <legend className="text-xl font-bold mb-4 text-title-blue-france">
+        Type de bâti
+      </legend>
+      <SelectWithValidation
+        name="typeBati"
+        control={control}
+        label=""
+        id="typeBati"
+        className="max-w-xs"
+      >
+        <option value="">Sélectionnez une option</option>
 
-      {Object.values(Repartition).map((repartition) => (
-        <option key={repartition} value={repartition}>
-          {repartition}
-        </option>
-      ))}
-    </SelectWithValidation>
+        {Object.values(Repartition).map((repartition) => (
+          <option key={repartition} value={repartition}>
+            {repartition}
+          </option>
+        ))}
+      </SelectWithValidation>
+    </fieldset>
   );
 };
